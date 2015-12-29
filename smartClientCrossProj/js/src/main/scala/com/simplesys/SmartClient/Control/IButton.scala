3b58688a9f286f31d053bdcf0c47dc94fb2c90c4
andrew <uandrew1965@gmail.com>
2015-12-29 21:24:20 +0300

    .

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Control/IButton.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Control/IButton.scala
@@ -1,10 +1,15 @@
 package com.simplesys.SmartClient.Control
 
+import com.simplesys.SmartClient.Control.inst._
+import com.simplesys.SmartClient.Control.props.IButtonProps
 import com.simplesys.SmartClient.Foundation.AbstractCanvasCompanion
+import com.simplesys.SmartClient.System.SCApply
+import com.simplesys.macros.PropsToMap
 
-import scala.scalajs.js
+import scala.scalajs.js.annotation.ScalaJSDefined
 
-@js.native
+@ScalaJSDefined
 object IButton extends AbstractCanvasCompanion {
-    type IButton = IButton.type
+    private val sCApply = new SCApply[IButton, IButtonProps]()
+    def create(props: IButtonProps): IButton = sCApply create props
 }
