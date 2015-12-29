771ba292ebdbd80830ed75c393a8be6b530bf725
andrew <uandrew1965@gmail.com>
2015-12-29 20:58:16 +0300

    .

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Foundation/props/StatefulCanvasProps.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Foundation/props/StatefulCanvasProps.scala
@@ -1,65 +1,44 @@
 package com.simplesys.SmartClient.Foundation.props
 
 import com.simplesys.SmartClient.Foundation.inst.Canvas
+import com.simplesys.SmartClient.option.{ScNone, ScOption}
 import com.simplesys.isc.System.Types.Alignment.Alignment
 import com.simplesys.isc.System.Types.SelectionType.SelectionType
 import com.simplesys.isc.System.Types.State._
 import com.simplesys.isc.System.Types.VerticalAlignment._
 import com.simplesys.isc.System.Types._
 
-import scala.scalajs.js
-
 class StatefulCanvasProps extends CanvasProps {
-
-//    var actionType: SelectionType
-//    def addToRadioGroup(groupID: String): Unit
-//    var align: Alignment
-//    var autoFit: Boolean
-//    var baseStyle: CSSStyleName
-//    def deselect(): Unit
-//    def getActionType(): Unit
-//    def getState(): State
-//    def getStateSuffix(): String
-//    def getTitle(): HTMLString
-//    var icon: SCImgURL
-//    val iconHeight: Int
-//    val iconOrientation: String
-//    val iconSize: Int
-//    val iconSpacing: Int
-//    var iconStyle: CSSStyleName
-//    val iconWidth: Int
-//    var ignoreRTL: Boolean
-//    def isSelected(): Boolean
-//    val overCanvas: Canvas with AutoChild
-//    var overCanvasConstructor: String
-//    var overCanvasDefaults: Canvas
-//    var radioGroup: String
-//    var redrawOnStateChange: Boolean
-//    def removeFromRadioGroup(groupID: String = js.native): Unit
-//    def select(): Unit
-//    val selected: Boolean
-//    def setActionType(): Unit
-//    def setAutoFit(autoFit: Boolean): Unit
-//    def setBaseStyle(style: CSSStyleName): Unit
-//    def setIcon(icon: URL): Unit
-//    def setIconOrientation(orientation: String): Unit
-//    def setIconStyle(iconStyle: CSSStyleName): Unit
-//    def setIgnoreRTL(ignoreRTL: Boolean): Unit
-//    def setSelected(newIsSelected: Boolean): Unit
-//    def setState(newState: State): Unit
-//    def setTitle(newTitle: HTMLString): Unit
-//    var showDisabled: Boolean
-//    var showDown: Boolean
-//    val showDownIcon: Boolean
-//    var showFocused: Boolean
-//    var showFocusedAsOver: Boolean
-//    var showFocusedIcon: Boolean
-//    var showOverCanvas: Boolean
-//    var showRollOver: Boolean
-//    var showRollOverIcon: Boolean
-//    var showRTLIcon: Boolean
-//    var showSelectedIcon: Boolean
-//    val state: State
-//    val title: HTMLString
-//    var valign: VerticalAlignment
+    val actionType: ScOption[SelectionType] = ScNone
+    val align: ScOption[Alignment] = ScNone
+    val autoFit: ScOption[Boolean] = ScNone
+    val baseStyle: ScOption[CSSStyleName] = ScNone
+    val icon: ScOption[SCImgURL] = ScNone
+    val iconHeight: ScOption[Int] = ScNone
+    val iconOrientation: ScOption[String] = ScNone
+    val iconSize: ScOption[Int] = ScNone
+    val iconSpacing: ScOption[Int] = ScNone
+    val iconStyle: ScOption[CSSStyleName] = ScNone
+    val iconWidth: ScOption[Int] = ScNone
+    val ignoreRTL: ScOption[Boolean] = ScNone
+    val overCanvas: ScOption[Canvas] = ScNone
+    val overCanvasConstructor: ScOption[String] = ScNone
+    val overCanvasDefaults: ScOption[Canvas] = ScNone
+    val radioGroup: ScOption[String] = ScNone
+    val redrawOnStateChange: ScOption[Boolean] = ScNone
+    val selected: ScOption[Boolean] = ScNone
+    val showDisabled: ScOption[Boolean] = ScNone
+    val showDown: ScOption[Boolean] = ScNone
+    val showDownIcon: ScOption[Boolean] = ScNone
+    val showFocused: ScOption[Boolean] = ScNone
+    val showFocusedAsOver: ScOption[Boolean] = ScNone
+    val showFocusedIcon: ScOption[Boolean] = ScNone
+    val showOverCanvas: ScOption[Boolean] = ScNone
+    val showRollOver: ScOption[Boolean] = ScNone
+    val showRollOverIcon: ScOption[Boolean] = ScNone
+    val showRTLIcon: ScOption[Boolean] = ScNone
+    val showSelectedIcon: ScOption[Boolean] = ScNone
+    val state: ScOption[State] = ScNone
+    val title: ScOption[HTMLString] = ScNone
+    val valign: ScOption[VerticalAlignment] = ScNone
 }
