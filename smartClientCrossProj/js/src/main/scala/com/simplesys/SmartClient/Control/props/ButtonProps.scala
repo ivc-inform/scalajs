6434fe804e60de6adf55f852813a6247060462f7
andrew <uandrew1965@gmail.com>
2015-12-29 21:20:06 +0300

    .

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Control/props/ButtonProps.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Control/props/ButtonProps.scala
@@ -1,7 +1,23 @@
 package com.simplesys.SmartClient.Control.props
 
 import com.simplesys.SmartClient.Foundation.props.StatefulCanvasProps
+import com.simplesys.SmartClient.option.{ScNone, ScOption}
+import com.simplesys.isc.System.Types.Cursor.Cursor
+import com.simplesys.isc.System.Types._
+
+import scala.scalajs.js
 
 class ButtonProps extends StatefulCanvasProps {
-
+    type buttonHandler <: this.type
+    var action: ScOption[Function[buttonHandler, Unit]] = ScNone
+    val disabledIconCursor: ScOption[Cursor] = ScNone
+    var hiliteAccessKey: ScOption[Boolean] = ScNone
+    val iconAlign: ScOption[String] = ScNone
+    var iconClick: ScOption[Function[buttonHandler, Boolean]] = ScNone
+    val iconCursor: ScOption[Cursor] = ScNone
+    var showClippedTitleOnHover: ScOption[Boolean] = ScNone
+    var showDisabledIcon: ScOption[Boolean] = ScNone
+    var titleClipped: ScOption[Function[buttonHandler, Boolean]] = ScNone
+    var titleHover: ScOption[Function[buttonHandler, Boolean]] = ScNone
+    var wrap: ScOption[Boolean] = ScNone
 }
