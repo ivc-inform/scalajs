b1f988d9657f56a50a16b37461b42d92c3e7269d
andrew <uandrew1965@gmail.com>
2015-12-31 07:46:56 +0300

    Все заново

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/System/Class.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/System/Class.scala
@@ -7,12 +7,7 @@ import scala.scalajs.js
 import inst._
 
 abstract class AbstractClass[T <: Class, P <: ClassProps](implicit classTag_T: ClassTag[T], classTag_P: ClassTag[P]) {
-
-    def create(propsClass: P): T = {
-        val className = classTag_T.runtimeClass.getClass.getSimpleName
-        println(className)
-        js.Dynamic.global.isc.selectDynamic(className).create().asInstanceOf[T]
-    }
+    def create(propsClass: P): T = js.Dynamic.global.isc.selectDynamic(classTag_T.runtimeClass.getSimpleName).create().asInstanceOf[T]
 }
 
 object Class extends AbstractClass[Class, ClassProps]
---
+++ smartClientCrossProj/js/src/test/scala/System/TestSuit.scala
@@ -0,0 +1,12 @@
+package System
+
+import com.simplesys.SmartClient.Control.IButton
+import org.scalatest.FunSuite
+
+class TestSuit extends FunSuite{
+    test("className") {
+//        println(com.simplesys.SmartClient.System.Class.className)
+//        println(IButton.className)
+    }
+
+}
