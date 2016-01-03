23d0d8c4e45f24e1659b702e39185aaeab26b833
andrew <uandrew1965@gmail.com>
2016-01-03 10:31:10 +0300

    Все заново

---
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/System/Selection/CellSelection.scala
@@ -0,0 +1,29 @@
+package com.simplesys.SmartClient.System.Selection
+
+import com.simplesys.SmartClient.System.Types._
+import com.simplesys.SmartClient.System.{Types, AbstractClassCompanion, Class}
+
+import scala.scalajs.js
+import scala.scalajs.js.|
+
+@js.native
+trait CellSelection extends Class {
+    def anySelected(): Boolean
+        def cellIsSelected(rowNum: Int, colNum: Int): Boolean
+        def deselectCell(rowNum: Int, colNum: Int): Boolean
+        def deselectCellList(list: js.Array[Types.Object]): Boolean
+        def getSelectedCells(): js.Array[Types.Object]
+        def getSelectedRecord(): ListGridRecord
+        def selectCell(rowNum: Int, colNum: Int): Boolean
+        def selectCellList(list: js.Array[Types.Object]): Boolean
+        def selectionChanged(): Unit
+        def selectSingleCell(rowNum: Int, colNum: Int): Boolean
+    
+}
+
+@js.native
+abstract trait AbstractCellSelectionCompanion extends AbstractClassCompanion {
+}
+
+@js.native
+object CellSelection extends AbstractCellSelectionCompanion        
