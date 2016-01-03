181cde2748244f76054433abc3c0838319c893fd
andrew <uandrew1965@gmail.com>
2016-01-03 10:31:43 +0300

    Все заново

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/System/Selection/CellSelection.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/System/Selection/CellSelection.scala
@@ -9,16 +9,15 @@ import scala.scalajs.js.|
 @js.native
 trait CellSelection extends Class {
     def anySelected(): Boolean
-        def cellIsSelected(rowNum: Int, colNum: Int): Boolean
-        def deselectCell(rowNum: Int, colNum: Int): Boolean
-        def deselectCellList(list: js.Array[Types.Object]): Boolean
-        def getSelectedCells(): js.Array[Types.Object]
-        def getSelectedRecord(): ListGridRecord
-        def selectCell(rowNum: Int, colNum: Int): Boolean
-        def selectCellList(list: js.Array[Types.Object]): Boolean
-        def selectionChanged(): Unit
-        def selectSingleCell(rowNum: Int, colNum: Int): Boolean
-    
+    def cellIsSelected(rowNum: Int, colNum: Int): Boolean
+    def deselectCell(rowNum: Int, colNum: Int): Boolean
+    def deselectCellList(list: js.Array[Types.Object]): Boolean
+    def getSelectedCells(): js.Array[Types.Object]
+    def getSelectedRecord(): ListGridRecord
+    def selectCell(rowNum: Int, colNum: Int): Boolean
+    def selectCellList(list: js.Array[Types.Object]): Boolean
+    def selectionChanged(): Unit
+    def selectSingleCell(rowNum: Int, colNum: Int): Boolean
 }
 
 @js.native
