d825095acf9f82353e070b44bd1375a9405f3af0
andrew <uandrew1965@gmail.com>
2016-01-03 19:01:07 +0300

    Все заново

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Layout/TabBar.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Layout/TabBar.scala
@@ -2,12 +2,26 @@ package com.simplesys.SmartClient.Layout
 
 
 import com.simplesys.SmartClient.Foundation.AbstractCanvasCompanion
+import com.simplesys.SmartClient.Layout.tabSet.Tab.Tab
+import com.simplesys.SmartClient.System.KeyIdentifier
+import com.simplesys.isc.System.Types.{SCClassName, SCImgURL}
 
 import scala.scalajs.js
 import scala.scalajs.js.|
 
 @js.native
 trait TabBar extends ToolBar {
+    val baseLineCapSize: Int
+    val baseLineSrc: SCImgURL
+    val baseLineThickness: Int
+    var breadth: Int
+    val closeTabKeys: js.Array[KeyIdentifier]
+    val defaultTabSize: Int
+    val moreTab: Tab
+    var moreTabCount: Int
+    def selectTab(tabNum: Int): Unit
+    val showMoreTab: Boolean
+    val tabs: js.Array[Tab]
 }
 
 @js.native
