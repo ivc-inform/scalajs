ea093ead68c950540a3f9359eee9423ae00ae57a
andrew <uandrew1965@gmail.com>
2016-01-04 06:57:52 +0300

    Все заново

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Layout/SectionStack.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Layout/SectionStack.scala
@@ -1,10 +1,15 @@
 package com.simplesys.SmartClient.Layout
 
+import com.simplesys.SmartClient.Foundation.Canvas
+import com.simplesys.SmartClient.Layout.sectionStack.SectionStackSection.SectionStackSection
+
 import scala.scalajs.js
 import scala.scalajs.js.|
 
 @js.native
 trait SectionStack extends VLayout {
+    def addItem(section: String | Int, item: Canvas, index: Int): Unit
+    def addSection (sections: SectionStackSection | js.Array[SectionStackSection], position:Int)
 }
 
 @js.native
