6d20c23553d24545ca850024a20673cb7f9b26dd
andrew <uandrew1965@gmail.com>
2016-01-06 00:03:03 +0300

    Все заново

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Grids/listGrid/groupNode.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Grids/listGrid/groupNode.scala
@@ -1,12 +1,16 @@
 package com.simplesys.SmartClient.Grids.listGrid
 
 import com.simplesys.SmartClient.System.Types
+import com.simplesys.isc.System.Types.HTMLString
 
 import scala.scalajs.js
-import scala.scalajs.js.|
+import scala.scalajs.js.{Array, |}
 
 @js.native
 trait GroupNode extends Types.Object {
+    val groupMembers: Array[ListGridRecord]
+    val groupTitle: HTMLString
+    val groupValue: js.Any
 }
 
 
