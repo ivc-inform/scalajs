4bd321f86504f059f7a13eb0205f7597e4b1a4b8
andrew <uandrew1965@gmail.com>
2016-01-05 22:50:03 +0300

    Все заново

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Grids/listGrid/Hilite.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Grids/listGrid/Hilite.scala
@@ -1,6 +1,9 @@
 package com.simplesys.SmartClient.Grids.listGrid
 
 import com.simplesys.SmartClient.System.Types
+import com.simplesys.SmartClient.System.Types.AdvancedCriteria.AdvancedCriteria
+import com.simplesys.SmartClient.System.Types.Criteria
+import com.simplesys.isc.System.Types.{SCImgURL, HTMLString}
 
 import scala.scalajs.js
 import scala.scalajs.js.|
@@ -8,4 +11,17 @@ import scala.scalajs.js.|
 @js.native
 object Hilite extends Types.Object {
     type Hilite = Hilite.type
+    var backgroundColor: String = js.native
+    val canEdit: Boolean = js.native
+    val criteria: Criteria | AdvancedCriteria = js.native
+    val cssText: String = js.native
+    var disabled: Boolean = js.native
+    var fieldName: String = js.native
+    var htmlAfter: HTMLString = js.native
+    var htmlBefore: HTMLString = js.native
+    val icon: SCImgURL = js.native
+    val id: String = js.native
+    val replacementValue: HTMLString = js.native
+    var textColor: String = js.native
+    var title:String = js.native
 }
