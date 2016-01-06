2e858eedf2da6126211a92a8ce89c34dc005b0dd
andrew <uandrew1965@gmail.com>
2016-01-06 05:55:02 +0300

    Все заново

---
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Grids/listGrid/GroupingMessages.scala
@@ -0,0 +1,45 @@
+package com.simplesys.SmartClient.Grids.listGrid
+
+import com.simplesys.SmartClient.System.AbstractClassCompanion
+import com.simplesys.SmartClient.System.Class
+
+import scala.scalajs.js
+import scala.scalajs.js.|
+
+@js.native
+trait GroupingMessages extends Class {
+}
+
+@js.native
+abstract trait AbstractGroupingMessagesCompanion extends AbstractClassCompanion {
+    var byDateTitle:String = js.native
+    var byDayOfMonthAndYearTitle:String  = js.native
+    var byDayOfMonthTitle:String  = js.native
+    var byDayOfWeekAndYearTitle:String  = js.native
+    var byDayTitle:String  = js.native
+    var byHoursTitle:String  = js.native
+    var byMillisecondsTitle:String  = js.native
+    var byMinutesTitle:String  = js.native
+    var byMonthAndYearTitle:String  = js.native
+    var byMonthTitle:String  = js.native
+    var byQuarterAndYearTitle:String  = js.native
+    var byQuarterTitle:String  = js.native
+    var bySecondsTitle:String  = js.native
+    var byUpcomingTitle:String  = js.native
+    var byWeekAndYearTitle:String  = js.native
+    var byWeekTitle:String  = js.native
+    var byYearTitle:String  = js.native
+    var timezoneMinutesSuffix:String  = js.native
+    var timezoneSecondsSuffix:String  = js.native
+    var upcomingBeforeTitle:String  = js.native
+    var upcomingLaterTitle:String  = js.native
+    var upcomingNextMonthTitle:String  = js.native
+    var upcomingNextWeekTitle:String  = js.native
+    var upcomingThisWeekTitle:String  = js.native
+    var upcomingTodayTitle:String  = js.native
+    var upcomingTomorrowTitle:String  = js.native
+    var weekNumberTitle:String  = js.native
+}
+
+@js.native
+object GroupingMessages extends AbstractGroupingMessagesCompanion        
