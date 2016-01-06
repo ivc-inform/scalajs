276fbbdb792f18acb3b0c995acfa4966d7ee1d6d
andrew <uandrew1965@gmail.com>
2016-01-06 16:38:06 +0300

    Все заново

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Forms/FormsItems/formItem/FormItemIcon.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Forms/FormsItems/formItem/FormItemIcon.scala
@@ -3,15 +3,37 @@ package com.simplesys.SmartClient.Forms.FormsItems.formItem
 import com.simplesys.SmartClient.Forms.DynamicForm
 import com.simplesys.SmartClient.Forms.FormsItems.FormItem
 import com.simplesys.SmartClient.System.Types
+import com.simplesys.isc.System.Types.Alignment.Alignment
 import com.simplesys.isc.System.Types.CharacterCasing.CharacterCasing
-import com.simplesys.isc.System.Types.{HTMLString, SCClassName}
+import com.simplesys.isc.System.Types.PickListItemIconPlacement.PickListItemIconPlacement
+import com.simplesys.isc.System.Types.{SCImgURL, CSSStyleName, HTMLString, SCClassName}
 
 import scala.scalajs.js
 import scala.scalajs.js.{Array, |}
 
 @js.native
 trait FormItemIcon extends Types.Object {
-
+    var baseStyle: CSSStyleName
+    var click: js.Function3[DynamicForm, FormItem, FormItemIcon, Unit]
+    var disabled: Boolean
+    var disableOnReadOnly: Boolean
+    var height: Int
+    var hspace: Int
+    var iconPlacement: PickListItemIconPlacement
+    val inline: Boolean
+    val inlineIconAlign: Alignment
+    var keyPress: js.Function5[String, Char, DynamicForm, FormItem, FormItemIcon, _]
+    val name: String
+    var neverDisable: Boolean
+    var prompt: HTMLString
+    var showFocused: Boolean
+    var showFocusedWithItem: Boolean
+    var showIf: js.Function2[DynamicForm, FormItem, Boolean]
+    var showOver: Boolean
+    var showRTL: Boolean
+    var src: SCImgURL
+    val tabIndex: Int
+    val width: Int
 }
 
 
---
+++ smartClientCrossProj/shared/src/main/scala/com/simplesys/isc/System/Types/PickListItemIconPlacement.scala
@@ -0,0 +1,8 @@
+package com.simplesys.isc.System.Types
+
+import language.implicitConversions
+
+object PickListItemIconPlacement extends Enumeration {
+    type PickListItemIconPlacement = Value
+    val pickerNavigationBar, formItem, both = Value
+}
