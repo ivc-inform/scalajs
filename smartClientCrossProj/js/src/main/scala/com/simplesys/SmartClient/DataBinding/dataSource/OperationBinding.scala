0c65003118a2141db7126e7290e1bed7d599666e
andrew <uandrew1965@gmail.com>
2016-01-09 21:56:52 +0300

    Все заново

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/DataBinding/dataSource/OperationBinding.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/DataBinding/dataSource/OperationBinding.scala
@@ -1,10 +1,16 @@
 package com.simplesys.SmartClient.DataBinding.dataSource
 
+import com.simplesys.SmartClient.DataBinding.{DataSource, DSRequest}
+import com.simplesys.SmartClient.RPC.ServerObject
 import com.simplesys.SmartClient.System.Types
+import com.simplesys.SmartClient.System.Types.{VelocityExpression, XPathExpression}
 import com.simplesys.isc.System.Types.DSDataFormat.DSDataFormat
+import com.simplesys.isc.System.Types.DSOperationType.DSOperationType
 import com.simplesys.isc.System.Types.DSProtocol.DSProtocol
 import com.simplesys.isc.System.Types.ExportFormat.ExportFormat
 import com.simplesys.isc.System.Types.RPCTransport.RPCTransport
+import com.simplesys.isc.System.Types.SQLPagingStrategy.SQLPagingStrategy
+import com.simplesys.isc.System.Types.SQLType.SQLType
 import com.simplesys.isc.System.Types.URL
 
 import scala.scalajs.js
@@ -29,7 +35,7 @@ trait OperationBinding extends Types.Object {
     val dataFormat: DSDataFormat
     val dataProtocol: DSProtocol
     val dataTransport: RPCTransport
-    val dataURL: URL                                                                                                        K
+    val dataURL: URL K
     val defaultParams: Types.Object
     val excludeCriteriaFields: Array[String]
     val exportAs: ExportFormat
@@ -42,5 +48,46 @@ trait OperationBinding extends Types.Object {
     val invalidateCache: Boolean
     val lineBreakStyle: String
     val mail: Mail
+    val methodArguments: String
+    val namedQuery: String
+    val operationId: String
+    val operationType: DSOperationType
+    var orderClause: String
+    var outputs: String
+    var ownerIdField: String
+    val preventHTTPCaching: Boolean
+    val progressiveLoading: Boolean
+    val providesMissingKeys: Boolean
+    val qualifyColumnNames: Boolean
+    val recordName: String
+    val recordXPath: XPathExpression
+    val requestProperties: DSRequest
+    val requires: VelocityExpression
+    val requiresAuthentication: Boolean
+    val requiresRole: String
+    val responseDataSchema: DataSource
+    val script: String
+    val selectClause: String
+    val serverMethod: String
+    val serverObject: ServerObject
+    val skipAudit: Boolean
+    val skipRowCount: Boolean
+    val spoofResponses: Boolean
+    val sqlPaging: SQLPagingStrategy
+    val sqlType: SQLType
+    val sqlUsePagingHint: Boolean
+    val summaryFunctions: Types.Object
+    val tableClause: String
+    val transformMultipleFields: Boolean
+    val useFlatFields: Boolean
+    val useForCacheSync: Boolean
+    val useHttpProxy: Boolean
+    val useSpringTransaction: Boolean
+    val useSubselectForRowCount: Boolean
+    val values: Array[DSRequestModifier]
+    val valuesClause: String
+    val whereClause: String
+    val wsOperation: String
+    val xmlNamespaces: Types.Object
 }
 
