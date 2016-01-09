50717f8af32ae682c9f47d9df3df0ac6c1bdf257
andrew <uandrew1965@gmail.com>
2016-01-09 07:34:08 +0300

    Все заново

---
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Control/RangeSlider.scala
@@ -0,0 +1,29 @@
+package com.simplesys.SmartClient.Control
+
+import com.simplesys.SmartClient.Foundation.{Scrollbar, Canvas, AbstractCanvasCompanion}
+import com.simplesys.SmartClient.Layout.Snapbar
+import com.simplesys.isc.System.Types.AutoChild
+
+import scala.scalajs.js
+import scala.scalajs.js.|
+
+@js.native
+trait RangeSlider extends Canvas {
+    var changed: js.Function3[Double, Double, Boolean, _]
+    val endThumb: Snapbar with AutoChild
+    var endValue: Double
+    var maxValue: Double
+    var minValue: Double
+    val scrollbar: Scrollbar with AutoChild
+    val startThumb: Snapbar with AutoChild
+    val startValue: Double
+    val track: Canvas with AutoChild
+    val vertical: Boolean
+}
+
+@js.native
+abstract trait AbstractRangeSliderCompanion extends AbstractCanvasCompanion {
+}
+
+@js.native
+object RangeSlider extends AbstractRangeSliderCompanion        
---
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Foundation/Scrollbar.scala
@@ -0,0 +1,15 @@
+package com.simplesys.SmartClient.Foundation
+
+import scala.scalajs.js
+import scala.scalajs.js.|
+
+@js.native
+trait Scrollbar extends StretchImg {
+}
+
+@js.native
+abstract trait AbstractScrollbarCompanion extends AbstractStretchImgCompanion {
+}
+
+@js.native
+object Scrollbar extends AbstractScrollbarCompanion        
