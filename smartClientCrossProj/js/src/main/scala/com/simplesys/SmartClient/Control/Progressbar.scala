08de114c40fe4b17ccdd5e90b4e9393fa1611944
andrew <uandrew1965@gmail.com>
2016-01-09 08:53:18 +0300

    Все заново

---
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Control/Progressbar.scala
@@ -0,0 +1,29 @@
+package com.simplesys.SmartClient.Control
+
+import com.simplesys.SmartClient.Foundation.{AbstractStretchImgCompanion, StretchImg}
+import com.simplesys.isc.System.Types.{SCImgURL, void}
+
+import scala.scalajs.js
+import scala.scalajs.js.|
+
+@js.native
+trait Progressbar extends StretchImg {
+    var breadth: Int
+    def getBreadth(): Int
+    def getLength(): Int
+    val length: Int
+    var percentChanged: js.Function1[void, _]
+    var percentDone: Int
+    def setBreadth(newBreadth: Int): void
+    def setLength(newLength: Int): void
+    def setPercentDone(newPercent: Int): void
+    var src: SCImgURL
+    var vertical:Boolean
+}
+
+@js.native
+abstract trait AbstractProgressbarCompanion extends AbstractStretchImgCompanion {
+}
+
+@js.native
+object Progressbar extends AbstractProgressbarCompanion        
