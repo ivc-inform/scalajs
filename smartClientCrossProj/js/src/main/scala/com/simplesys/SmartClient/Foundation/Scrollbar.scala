c2bf2381939d5f4f7c714cfc31ca7ee3321e1787
andrew <uandrew1965@gmail.com>
2016-01-09 18:29:27 +0300

    Все заново

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Foundation/Scrollbar.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Foundation/Scrollbar.scala
@@ -1,10 +1,36 @@
 package com.simplesys.SmartClient.Foundation
 
+import com.simplesys.isc.System.Types.{URL, void, SCImgURL}
+
 import scala.scalajs.js
 import scala.scalajs.js.|
 
 @js.native
 trait Scrollbar extends StretchImg {
+    val allowThumbDownState: Boolean
+    val allowThumbOverState:Boolean
+    var autoEnable:Boolean
+    var btnSize:Int
+    val cornerImg: StretchItem
+    val cornerSize:Int
+    val cornerSrc: SCImgURL
+    val endImg:StretchImg
+    val endThumbOverlap:Int
+    val scrollTarget:Canvas
+    def setScrollTarget (newTarget:Canvas = js.native):void
+    val showCorner:Boolean
+    val showTrackButtons:Boolean
+    val showTrackEnds:Boolean
+    val startImg:StretchItem
+    val startThumbOverlap:Int
+    val thumbInset:Int
+    val thumbMinSize:Int
+    val thumbOverlap:Int
+    val trackEndHeight:Int
+    val trackEndImg:StretchItem
+    val trackEndWidth:Int
+    val trackImg: StretchItem
+    val trackStartImg: StretchItem
 }
 
 @js.native
---
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Foundation/scrollbar/ScrollThumb.scala
@@ -0,0 +1,19 @@
+package com.simplesys.SmartClient.Foundation.scrollbar
+
+import com.simplesys.SmartClient.Foundation.{AbstractStretchImgCompanion, StretchImg}
+
+import scala.scalajs.js
+import scala.scalajs.js.|
+
+@js.native
+trait ScrollThumb extends StretchImg {
+    var capSize:Int
+    val src
+}
+
+@js.native
+abstract trait AbstractScrollThumbCompanion extends AbstractStretchImgCompanion {
+}
+
+@js.native
+object ScrollThumb extends AbstractScrollThumbCompanion        
