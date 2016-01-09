b3be693b800e1c214827f725719670c2d213f2dd
andrew <uandrew1965@gmail.com>
2016-01-09 07:38:03 +0300

    Все заново

---
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Control/Hover.scala
@@ -0,0 +1,25 @@
+package com.simplesys.SmartClient.Control
+
+import com.simplesys.SmartClient.Foundation.{Label, Canvas}
+import com.simplesys.SmartClient.System.{Types, AbstractClassCompanion, Class}
+import com.simplesys.isc.System.Types.{void, HTMLString}
+
+import scala.scalajs.js
+import scala.scalajs.js.|
+
+@js.native
+trait Hover extends Class {
+}
+
+@js.native
+abstract trait AbstractHoverCompanion extends AbstractClassCompanion {
+    def hide():void = js.native
+    var hoverCanvasDefaults: Types.Object = js.native
+    var leftOffset: Int = js.native
+    val moveWithMouse: Boolean = js.native
+    def show(contents: HTMLString | Canvas, properties: Label): void = js.native
+    var topOffset: Int = js.native
+}
+
+@js.native
+object Hover extends AbstractHoverCompanion        
