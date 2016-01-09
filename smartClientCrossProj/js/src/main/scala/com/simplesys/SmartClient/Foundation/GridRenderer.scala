95b957e09df766d11ec063790d7c573de009b914
andrew <uandrew1965@gmail.com>
2016-01-09 16:50:28 +0300

    Все заново

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Foundation/GridRenderer.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Foundation/GridRenderer.scala
@@ -1,10 +1,108 @@
 package com.simplesys.SmartClient.Foundation
 
+import com.simplesys.SmartClient.Grids.listGrid.ListGridRecord
+import com.simplesys.SmartClient.System.Types
+import com.simplesys.isc.System.Types.State.State
+import com.simplesys.isc.System.Types.{void, HTMLString, CSSStyleName}
+import org.scalajs.dom
+import org.scalajs.dom.Element
+import org.scalajs.dom.html.Document
+
 import scala.scalajs.js
-import scala.scalajs.js.|
+import scala.scalajs.js.{Any, |}
 
 @js.native
 trait GridRenderer extends Canvas {
+    var alternateColumnFrequency: Int
+    var alternateColumnStyles: Boolean
+    var alternateRowFrequency: Int
+    val alternateRowStyles: Boolean
+    var autoFit: Boolean
+    val baseStyle: CSSStyleName
+    var canSelectOnRightMouse: Boolean
+    var cellClick: js.Function3[ListGridRecord, Int, Int, Boolean]
+    var cellContextClick: js.Function3[ListGridRecord, Int, Int, Boolean]
+    var cellDoubleClick: js.Function3[ListGridRecord, Int, Int, Boolean]
+    var cellHeight: Int
+    var cellHover: js.Function3[ListGridRecord, Int, Int, Boolean]
+    var cellHoverHTML: js.Function3[ListGridRecord, Int, Int, HTMLString]
+    var cellIsEnabled: js.Function2[Int, Int, Boolean]
+    var cellMouseDown: js.Function3[ListGridRecord, Int, Int, Boolean]
+    var cellMouseUp: js.Function3[ListGridRecord, Int, Int, Boolean]
+    var cellOut: js.Function3[ListGridRecord, Int, Int, Boolean]
+    var cellOver: js.Function3[ListGridRecord, Int, Int, Boolean]
+    var cellPadding: Int
+    var cellSelectionChanged: js.Function1[js.Array[Types.Object], Boolean]
+    var cellValueHover: js.Function3[ListGridRecord, Int, Int, Boolean]
+    var cellValueHoverHTML: js.Function3[ListGridRecord, Int, Int, HTMLString]
+    var cellValueIsClipped: js.Function2[Int, Int, Boolean]
+    var drawAheadRatio: Double
+    var drawAllMaxCells: Int
+    var emptyCellValue: String
+    var emptyMessage: String
+    var emptyMessageStyle: CSSStyleName
+    var emptyMessageTableStyle: CSSStyleName
+    var fastCellUpdates: Boolean
+    var findColNum: js.Function1[ListGridRecord, Int]
+    var findRowNum: js.Function1[ListGridRecord, Int]
+    var fixedColumnWidths: Boolean
+    var fixedRowHeights: Boolean
+    var getBaseStyle: js.Function3[ListGridRecord, Int, Int, Boolean]
+    var getCellCSSText: js.Function3[ListGridRecord, Int, Int, Boolean]
+    def getCellFromDomElement(element: dom.Element): js.Array[Int]
+    var getCellHoverComponent: js.Function3[ListGridRecord, Int, Int, Boolean]
+    def getCellPageRect(rowNum: Int, colNum: Int): js.Array[Int]
+    var getCellRecord: js.Function2[Int, Int, Types.Object]
+    def getCellRowSpan(rowNum: Int, colNum: Int): Int
+    def getCellStartRow(rowNum: Int, colNum: Int): Int
+    var getCellStyle: js.Function3[ListGridRecord, Int, Int, CSSStyleName]
+    var getCellValue: js.Function3[ListGridRecord, Int, Int, String]
+    def getColumnLeft(colNum: Int): Int
+    def getColumnPageLeft(colNum: Int): Int
+    def getColumnWidth(colNum: Int): Int
+    def getDrawnRows(): js.Array[Int]
+    def getEventColumn(x: Int = js.native): Int
+    def getEventRow(y: Int = js.native): Int
+    def getNearestColToEvent(): Int
+    def getNearestRowToEvent(): Int
+    var getRowHeight: js.Function2[ListGridRecord, Int, Int]
+    def getRowPageTop(rowNum: Int): Int
+    var getRowSpan: js.Function3[ListGridRecord, Int, Int, Int]
+    def getRowTop(rowNum: Int): Int
+    def getTotalRows(): Int
+    def getVisibleRows(): Int
+    var instantScrollTrackRedraw: Boolean
+    var offlineMessage: String
+    var offlineMessageStyle: CSSStyleName
+    var quickDrawAheadRatio: Double
+    var recordCustomStyleProperty: String
+    def refreshCell(rowNum: Int, colNum: Int): void
+    def refreshCellStyle(rowNum: Int, colNum: Int): void
+    def refreshRow(rowNum: Int): void
+    var rowClick: js.Function3[ListGridRecord, Int, Int, Boolean]
+    var rowContextClick: js.Function3[ListGridRecord, Int, Int, Boolean]
+    var rowDoubleClick: js.Function3[ListGridRecord, Int, Int, Boolean]
+    var rowHover: js.Function3[ListGridRecord, Int, Int, Boolean]
+    var rowMouseDown: js.Function3[ListGridRecord, Int, Int, Boolean]
+    var rowMouseUp: js.Function3[ListGridRecord, Int, Int, Boolean]
+    var rowOut: js.Function3[ListGridRecord, Int, Int, Boolean]
+    var rowOver: js.Function3[ListGridRecord, Int, Int, Boolean]
+    var scrollRedrawDelay: Int
+    var scrollWheelRedrawDelay: Int
+    var selectionChanged: js.Function2[ListGridRecord, State, _]
+    def setColumnWidth(colNum: Int, newWidth: Int): void
+    def setColumnWidths(newWidths: js.Array[Int]): void
+    def setFastCellUpdates(fastCellUpdates: Boolean): void
+    val showAllColumns: Boolean
+    val showAllRows: Boolean
+    val showClippedValuesOnHover: Boolean
+    val showEmptyMessage: Boolean
+    val showOfflineMessage: Boolean
+    val snapInsideBorder: Boolean
+    var totalRows: Int
+    var touchScrollRedrawDelay: Int
+    val virtualScrolling: Boolean
+    val wrapCells: Boolean
 }
 
 @js.native
