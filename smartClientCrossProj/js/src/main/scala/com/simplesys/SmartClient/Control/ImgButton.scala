a47e6199a8130ba5573354c29bfab96e073a0180
andrew <uandrew1965@gmail.com>
2016-01-09 14:11:09 +0300

    Все заново

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Control/ImgButton.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Control/ImgButton.scala
@@ -1,12 +1,27 @@
 package com.simplesys.SmartClient.Control
 
 import com.simplesys.SmartClient.Foundation.{AbstractCanvasCompanion, Img}
+import com.simplesys.isc.System.Types.State.State
+import com.simplesys.isc.System.Types.VerticalAlignment.VerticalAlignment
+import com.simplesys.isc.System.Types.{SCImgURL, URL, CSSStyleName, void}
 
 import scala.scalajs.js
 import scala.scalajs.js.|
 
 @js.native
 trait ImgButton extends Img {
+    var action: js.Function1[void, _]
+    var hiliteAccessKey: Boolean
+    var iconAlign: String
+    var labelHPad: Int
+    var labelVPad: Int
+    def setSelected(): void
+    var showClippedTitleOnHover: Boolean
+    var showDisabledIcon: Boolean
+    var showTitle: Boolean
+    var src: SCImgURL
+    var titleClipped: js.Function1[void, Boolean]
+    var titleHover: js.Function1[void, Boolean]
 }
 
 @js.native
