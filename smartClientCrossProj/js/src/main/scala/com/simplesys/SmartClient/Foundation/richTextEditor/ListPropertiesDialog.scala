c34b08c4f785f68c76a3b8ef721378ab5c6ec939
andrew <uandrew1965@gmail.com>
2016-01-09 14:36:11 +0300

    Все заново

---
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Foundation/richTextEditor/ListPropertiesDialog.scala
@@ -0,0 +1,26 @@
+package com.simplesys.SmartClient.Foundation.richTextEditor
+
+import com.simplesys.SmartClient.Control.IButton
+import com.simplesys.SmartClient.Layout.{AbstractWindowCompanion, Window}
+import com.simplesys.isc.System.Types.AutoChild
+
+import scala.scalajs.js
+import scala.scalajs.js.|
+
+@js.native
+trait ListPropertiesDialog extends Window {
+    val applyButton:IButton with AutoChild
+    val applyButtonTitle:String
+    var applyClick:js.Function1[ListProperties, _]
+    val cancelButton:IButton with AutoChild
+    val cancelButtonTitle:String
+    var cancelClick:js.Function1[ListProperties, _]
+    val listPropertiesPane:ListPropertiesPane with AutoChild
+}
+
+@js.native
+abstract trait AbstractListPropertiesDialogCompanion extends AbstractWindowCompanion {
+}
+
+@js.native
+object ListPropertiesDialog extends AbstractListPropertiesDialogCompanion        
