cc45b1788713e9e41b1d0457d863d85ac62dadcf
andrew <uandrew1965@gmail.com>
2016-01-09 06:09:15 +0300

    Все заново

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Control/Dialog.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Control/Dialog.scala
@@ -1,12 +1,25 @@
 package com.simplesys.SmartClient.Control
 
-import com.simplesys.SmartClient.Layout.Window
+import com.simplesys.SmartClient.Layout.{AbstractWindowCompanion, Window}
+import com.simplesys.isc.System.Types.{SCImgURL, void}
 
 import scala.scalajs.js
+import scala.scalajs.js.{Array, |}
 
 @js.native
 trait Dialog extends Window {
+    def applyClick():void
+    val askIcon:SCImgURL
+    val autoFocus:Boolean
+    var buttonClick:js.Function2[Button, Int, _]
+    val buttons: Array[Button]
+    var cancelClick:js.Function1[void, _]
+    var closeClick:js.Function1[void, Boolean]
 }
 
+@js.native
+abstract trait AbstractDialogCompanion extends AbstractWindowCompanion {
+}
 
-
+@js.native
+object Dialog extends AbstractDialogCompanion        
