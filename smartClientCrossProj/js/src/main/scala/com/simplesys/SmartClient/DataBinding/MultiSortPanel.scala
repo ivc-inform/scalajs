134af72e3b57714ef6dc5878cd5122c70434440b
andrew <uandrew1965@gmail.com>
2016-01-10 02:20:06 +0300

    Все заново

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/DataBinding/MultiSortPanel.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/DataBinding/MultiSortPanel.scala
@@ -1,15 +1,44 @@
 package com.simplesys.SmartClient.DataBinding
 
 import com.simplesys.SmartClient.Control.IButton
+import com.simplesys.SmartClient.Grids.ListGrid
+import com.simplesys.SmartClient.Grids.listGrid.{ListGridRecord, ListGridField}
 import com.simplesys.SmartClient.Layout.{AbstractLayoutCompanion, Layout}
-import com.simplesys.isc.System.Types.AutoChild
+import com.simplesys.SmartClient.System.Types
+import com.simplesys.SmartClient.System.Types.Object
+import com.simplesys.isc.System.Types.{HTMLString, AutoChild}
 
 import scala.scalajs.js
-import scala.scalajs.js.|
+import scala.scalajs.js.{Array, |}
 
 @js.native
 trait MultiSortPanel extends Layout {
-    val addLevelButton:IButton with AutoChild
+    val addLevelButton: IButton with AutoChild
+    val addLevelButtonTitle: String
+    val ascendingTitle: String
+    val copyLevelButton: IButton with AutoChild
+    val copyLevelButtonTitle: String
+    val deleteLevelButton: IButton with AutoChild
+    val deleteLevelButtonTitle: String
+    val descendingTitle: String
+    val directionFieldTitle: String
+    val fields: Array[Object]
+    val firstSortLevelTitle: String
+    def getNumLevels(): Int
+    def getSort(): Array[SortSpecifier]
+    def getSortLevel(levelNum: Int): SortSpecifier
+    val initialSort: Array[SortSpecifier]
+    val invalidListPrompt: HTMLString
+    val levelDownButton: IButton with AutoChild
+    val levelDownButtonTitle: String
+    val levelUpButton: IButton with AutoChild
+    val levelUpButtonTitle: String
+    val maxLevels: Int
+    val optionsGrid: ListGrid[ListGridField, ListGridRecord]
+    val otherSortLevelTitle: String
+    val propertyFieldTitle: String
+    def sortChanged (sortLevels: Array[SortSpecifier])
+    def validate(): Boolean
 }
 
 @js.native
