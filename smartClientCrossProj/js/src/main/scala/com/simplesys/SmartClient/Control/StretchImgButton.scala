d0ac4a485506defd515c389c828bcc177715008d
andrew <uandrew1965@gmail.com>
2016-01-09 14:23:28 +0300

    Все заново

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Control/StretchImgButton.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Control/StretchImgButton.scala
@@ -1,12 +1,36 @@
 package com.simplesys.SmartClient.Control
 
 import com.simplesys.SmartClient.Foundation.{AbstractStretchImgCompanion, StretchImg}
+import com.simplesys.isc.System.Types.State.State
+import com.simplesys.isc.System.Types._
 
 import scala.scalajs.js
 import scala.scalajs.js.|
 
 @js.native
 trait StretchImgButton extends StretchImg {
+    var action: js.Function1[void, _]
+    var capSize: Int
+    var hiliteAccessKey: Boolean
+    var iconAlign: String
+    var iconClick: js.Function1[void, Boolean]
+    var labelBreadthPad: Int
+    var labelHPad: Int
+    var labelLengthPad: Int
+    var labelSkinImgDir: URL
+    var labelVPad: Int
+    def setLabelSkinImgDir(orientation: URL): void
+    def setSelected(): void
+    def setTitleStyle(style: CSSStyleName): void
+    var showClippedTitleOnHover: Boolean
+    var showDisabledIcon: Boolean
+    var showTitle: Boolean
+    var titleClipped: js.Function1[void, Boolean]
+    var titleHover: js.Function1[void, Boolean]
+    def titleHoverHTML (defaultHTML:HTMLString):HTMLString
+    var titleStyle:CSSStyleName
+    var vertical:Boolean
+    var wrap:Boolean
 }
 
 @js.native
