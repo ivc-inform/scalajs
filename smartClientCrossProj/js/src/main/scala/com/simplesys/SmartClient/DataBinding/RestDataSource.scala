a40d5a8ce9720eeb820227e82bbd969d852291c8
andrew <uandrew1965@gmail.com>
2016-01-10 03:46:23 +0300

    Все заново

---
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/DataBinding/RestDataSource.scala
@@ -0,0 +1,30 @@
+package com.simplesys.SmartClient.DataBinding
+
+import com.simplesys.SmartClient.DataBinding.dataSource.OperationBinding
+import com.simplesys.SmartClient.System.Types.XMLDocument
+import com.simplesys.isc.System.Types.void
+
+import scala.scalajs.js
+import scala.scalajs.js.{Array, |}
+
+@js.native
+trait RestDataSource extends DataSource {
+    val addDataURL: String
+    val disableQueuing: Boolean
+    val fetchDataURL: String
+    val jsonRecordXPath: String
+    val metaDataPrefix: String
+    val prettyPrintJSON: Boolean
+    val removeDataURL: String
+    val sendMetaData: Boolean
+    var transformRequest: js.Function1[void, _]
+    var updateDataURL: String
+    val xmlRecordXPath: String
+}
+
+@js.native
+abstract trait AbstractRestDataSourceCompanion extends AbstractDataSourceCompanion {
+}
+
+@js.native
+object RestDataSource extends AbstractRestDataSourceCompanion        
