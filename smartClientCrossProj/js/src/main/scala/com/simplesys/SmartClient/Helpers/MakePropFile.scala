c118e30803d47d871e00950dc3d08e1ac3d5ca2f
andrew <uandrew1965@gmail.com>
2016-01-11 17:28:07 +0300

    .

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Helpers/MakePropFile.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Helpers/MakePropFile.scala
@@ -4,24 +4,26 @@ import com.simplesys.io._
 
 import scala.io.Codec._
 import scala.io.Source
-import scala.collection.JavaConversions
-import scala.util.matching.Regex.Match
 
 object MakePropFile extends App {
     val f = "/home/andrew/JOB/scalajs/smartClientCrossProj/js/src/main/resources/Fasade.rc".toFile
+    val fres = "/home/andrew/JOB/scalajs/smartClientCrossProj/js/src/main/resources/Props.rc".toFile
 
     val rExp = """(?:\w+)(?:\s+)(\w+)\s*(?:\:)\s*(.*)""".r("name", "type")
 
-    Source.fromFile(f)(UTF8)getLines() foreach {
-        line =>
-         for (x<- rExp.findAllMatchIn(line)){
-             val name = x.group("name")
-             val `type` = x.group("type").replace("with AutoChild", "")
+    fres <=| {
+        out =>
+            Source.fromFile(f)(UTF8) getLines() foreach {
+                line =>
+                    for (x <- rExp.findAllMatchIn(line)) {
+                        val name = x.group("name")
+                        val `type` = x.group("type").replace("with AutoChild", "")
 
-             val res = s"var $name: ScOption[${`type`}] = ScNone"
+                        val res = s"var $name: ScOption[${`type`}] = ScNone"
 
-             println(res)
-         }
+                        out(res)
+                    }
 
+            }
     }
 }
