d0309acfd5ee2a0b615af6329c79c6ac8a2507e9
andrew <uandrew1965@gmail.com>
2016-01-11 18:55:21 +0300

    .

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Foundation/props/CanvasProps.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Foundation/props/CanvasProps.scala
@@ -2,29 +2,23 @@ package com.simplesys.SmartClient.Foundation.props
 
 import com.simplesys.SmartClient.Control.Menu
 import com.simplesys.SmartClient.Forms.FormsItems.CanvasItem
-import com.simplesys.SmartClient.Forms.ValuesManager
 import com.simplesys.SmartClient.Foundation.Canvas
+import com.simplesys.SmartClient.System.Types
 import com.simplesys.SmartClient.System.Types._
 import com.simplesys.SmartClient.System.props.ClassProps
 import com.simplesys.SmartClient.Tools.inst.EditNode
 import com.simplesys.SmartClient.option.{ScNone, ScOption}
-import com.simplesys.isc.System.Types.Alignment._
+import com.simplesys.isc.System.Types.Alignment.Alignment
 import com.simplesys.isc.System.Types.AnimationAcceleration.AnimationAcceleration
 import com.simplesys.isc.System.Types.BackgroundRepeat._
 import com.simplesys.isc.System.Types.Cursor._
 import com.simplesys.isc.System.Types.DragAppearance._
 import com.simplesys.isc.System.Types.DragIntersectStyle._
 import com.simplesys.isc.System.Types.DragMaskType._
-import com.simplesys.isc.System.Types.DrawPosition._
-import com.simplesys.isc.System.Types.EdgeName._
-import com.simplesys.isc.System.Types.LocatorStrategy._
-import com.simplesys.isc.System.Types.LocatorTypeStrategy._
-import com.simplesys.isc.System.Types.Overflow._
-import com.simplesys.isc.System.Types.PercentBoxModel._
-import com.simplesys.isc.System.Types.Positioning._
-import com.simplesys.isc.System.Types.ProportionalResizeMode._
-import com.simplesys.isc.System.Types.VerticalAlignment._
-import com.simplesys.isc.System.Types.Visibility._
+import com.simplesys.isc.System.Types.DrawPosition.DrawPosition
+import com.simplesys.isc.System.Types.LocatorStrategy.LocatorStrategy
+import com.simplesys.isc.System.Types.LocatorTypeStrategy.LocatorTypeStrategy
+import com.simplesys.isc.System.Types.VerticalAlignment.VerticalAlignment
 import com.simplesys.isc.System.Types._
 import org.scalajs.dom.raw.HTMLElement
 
@@ -121,146 +115,146 @@ class CanvasProps extends ClassProps {
     var dropMove: ScOption[js.ThisFunction0[callbackHandler, Boolean]] = ScNone
     var dropOut: ScOption[js.ThisFunction0[callbackHandler, Boolean]] = ScNone
     var dropOver: ScOption[js.ThisFunction0[callbackHandler, Boolean]] = ScNone
-    var dropTypes: ScOption[Either[js.Array[String],String]] = ScNone
-//    var dynamicContents: ScOption[Boolean] = ScNone
-//    var dynamicContentsVars: ScOption[ValueMap] = ScNone
-//    var edgeBackgroundColor: ScOption[Color] = ScNone
-//    var edgeCenterBackgroundColor: ScOption[Color] = ScNone
-//    var edgeCursorMap: ScOption[Types.Object] = ScNone
-//    var edgeImage: ScOption[SCImgURL] = ScNone
-//    var edgeMarginSize: ScOption[Int] = ScNone
-//    var edgeOffset: ScOption[Int] = ScNone
-//    var edgeOpacity: ScOption[Int] = ScNone
-//    var edgeShowCenter: ScOption[Boolean] = ScNone
-//    var edgeSize: ScOption[Int] = ScNone
-//    var editNode: ScOption[EditNode] = ScNone
-//    var editProxy: ScOption[EditNode] = ScNone
-//    var editProxyConstructor: ScOption[SCClassName] = ScNone
-//    var extraSpace: ScOption[Int] = ScNone
-//    var focusChanged: ScOption[js.ThisFunction1[callbackHandler, Boolean, void]] = ScNone
-//    var groupBorderCSS: ScOption[String] = ScNone
-//    var groupLabelBackgroundColor: ScOption[CSSColor] = ScNone
-//    var groupLabelStyleName: ScOption[CSSStyleName] = ScNone
-//    var groupTitle: ScOption[HTMLString] = ScNone
-//    var height: ScOption[String | Int] = ScNone
-//    var hideUsingDisplayNone: ScOption[Boolean] = ScNone
-//    var hover: ScOption[js.ThisFunction0[callbackHandler, Boolean]] = ScNone
-//    var hoverAlign: ScOption[Alignment] = ScNone
-//    var hoverAutoDestroy: ScOption[Boolean] = ScNone
-//    var hoverDelay: ScOption[Int] = ScNone
-//    var hoverHeight: ScOption[Int] = ScNone
-//    var hoverMoveWithMouse: ScOption[Boolean] = ScNone
-//    var hoverOpacity: ScOption[Int] = ScNone
-//    var hoverStyle: ScOption[CSSStyleName] = ScNone
-//    var hoverVAlign: ScOption[VerticalAlignment] = ScNone
-//    var hoverWidth: ScOption[Int] = ScNone
-//    var hoverWrap: ScOption[Boolean] = ScNone
-//    var htmlElement: ScOption[HTMLElement] = ScNone
-//    var htmlPosition: ScOption[DrawPosition] = ScNone
-//    var ID: ScOption[String] = ScNone
-//    var initWidget: ScOption[js.ThisFunction0[callbackHandler, void]] = ScNone
-//    var isGroup: ScOption[Boolean] = ScNone
-//    var isPrinting: ScOption[Boolean] = ScNone
-//    var isSnapAlignCandidate: ScOption[Boolean] = ScNone
-//    var keepInParentRect: ScOption[Boolean | js.Array[Int]] = ScNone
-//    var keyDown: ScOption[js.ThisFunction0[callbackHandler, Boolean]] = ScNone
-//    var keyPress: ScOption[js.ThisFunction0[callbackHandler, Boolean]] = ScNone
-//    var keyUp: ScOption[js.ThisFunction0[callbackHandler, Boolean]] = ScNone
-//    var layoutAlign: ScOption[Alignment | VerticalAlignment] = ScNone
-//    var leavePageSpace: ScOption[Int] = ScNone
-//    var left: ScOption[String | Int] = ScNone
-//    var locateChildrenBy: ScOption[LocatorStrategy] = ScNone
-//    var locateChildrenType: ScOption[LocatorTypeStrategy] = ScNone
-//    var locatePeersBy: ScOption[LocatorStrategy] = ScNone
-//    var locatePeersType: ScOption[LocatorTypeStrategy] = ScNone
-//    var margin: ScOption[Int] = ScNone
-//    var masterElement: ScOption[Canvas] = ScNone
-//    var matchElement: ScOption[Boolean] = ScNone
-//    var maxHeight: ScOption[Int] = ScNone
-//    var maxWidth: ScOption[Int] = ScNone
-//    var menuConstructor: ScOption[SCClassName] = ScNone
-//    var minHeight: ScOption[Int] = ScNone
-//    var minNonEdgeSize: ScOption[Int] = ScNone
-//    var minWidth: ScOption[Int] = ScNone
-//    var momentumScrollMinSpeed: ScOption[Int] = ScNone
-//    var mouseDown: ScOption[js.ThisFunction0[callbackHandler, Boolean]] = ScNone
-//    var mouseMove: ScOption[js.ThisFunction0[callbackHandler, Boolean]] = ScNone
-//    var mouseOut: ScOption[js.ThisFunction0[callbackHandler, Boolean]] = ScNone
-//    var mouseOver: ScOption[js.ThisFunction0[callbackHandler, Boolean]] = ScNone
-//    var mouseStillDown: ScOption[js.ThisFunction0[callbackHandler, Boolean]] = ScNone
-//    var mouseStillDownDelay: ScOption[Int] = ScNone
-//    var mouseStillDownInitialDelay: ScOption[Int] = ScNone
-//    var mouseUp: ScOption[js.ThisFunction0[callbackHandler, Boolean]] = ScNone
-//    var mouseWheel: ScOption[js.ThisFunction0[callbackHandler, Boolean]] = ScNone
-//    var moved: ScOption[js.ThisFunction2[callbackHandler, Int, Int, void]] = ScNone
-//    var noDoubleClicks: ScOption[Boolean] = ScNone
-//    var opacity: ScOption[Int] = ScNone
-//    var overflow: ScOption[Overflow] = ScNone
-//    var padding: ScOption[Int] = ScNone
-//    var parentCanvas: ScOption[Canvas] = ScNone
-//    var parentMoved: ScOption[js.ThisFunction2[callbackHandler, Int, Int, void]] = ScNone
-//    var peers: ScOption[js.Array[Canvas]] = ScNone
-//    var percentBox: ScOption[PercentBoxModel] = ScNone
-//    var percentSource: ScOption[Canvas] = ScNone
-//    var position: ScOption[Positioning] = ScNone
-//    var printChildrenAbsolutelyPositioned: ScOption[Boolean] = ScNone
-//    var prompt: ScOption[HTMLString] = ScNone
-//    var proportionalResizeModifiers: ScOption[js.Array[KeyName]] = ScNone
-//    var proportionalResizing: ScOption[ProportionalResizeMode] = ScNone
-//    var redrawOnResize: ScOption[Boolean] = ScNone
-//    var resizeBarTarget: ScOption[String] = ScNone
-//    var resized: ScOption[js.ThisFunction0[callbackHandler, Boolean]] = ScNone
-//    var resizeFrom: ScOption[js.Array[EdgeName]] = ScNone
-//    var rightMouseDown: ScOption[js.ThisFunction0[callbackHandler, Boolean]] = ScNone
-//    var scrollbarConstructor: ScOption[String] = ScNone
-//    var scrollbarSize: ScOption[Int] = ScNone
-//    var scrolled: ScOption[js.ThisFunction0[callbackHandler, Boolean]] = ScNone
-//    var setDragTracker: ScOption[js.ThisFunction0[callbackHandler, Boolean]] = ScNone
-//    var shadowDepth: ScOption[Int] = ScNone
-//    var shadowImage: ScOption[Int] = ScNone
-//    var shadowOffset: ScOption[Int] = ScNone
-//    var shadowSoftness: ScOption[Int] = ScNone
-//    var shouldPrint: ScOption[Boolean] = ScNone
-//    var showCustomScrollbars: ScOption[Boolean] = ScNone
-//    var showDragShadow: ScOption[Boolean] = ScNone
-//    var showEdges: ScOption[Boolean] = ScNone
-//    var showHover: ScOption[Boolean] = ScNone
-//    var showHoverComponents: ScOption[Boolean] = ScNone
-//    var showResizeBar: ScOption[Boolean] = ScNone
-//    var showShadow: ScOption[Boolean] = ScNone
-//    var showSnapGrid: ScOption[Boolean] = ScNone
-//    var shrinkElementOnHide: ScOption[Boolean] = ScNone
-//    var skinImgDir: ScOption[URL] = ScNone
-//    var snapAlignCandidates: ScOption[js.Array[Canvas]] = ScNone
-//    var snapAlignCenterLineStyle: ScOption[String] = ScNone
-//    var snapAlignEdgeLineStyle: ScOption[String] = ScNone
-//    var snapAxis: ScOption[String] = ScNone
-//    var snapEdge: ScOption[String] = ScNone
-//    var snapHDirection: ScOption[String] = ScNone
-//    var snapHGap: ScOption[Int] = ScNone
-//    var snapOffsetLeft: ScOption[Int] = ScNone
-//    var snapOffsetTop: ScOption[Int] = ScNone
-//    var snapOnDrop: ScOption[Int] = ScNone
-//    var snapResizeToAlign: ScOption[Int] = ScNone
-//    var snapResizeToGrid: ScOption[Int] = ScNone
-//    var snapTo: ScOption[String] = ScNone
-//    var snapToAlign: ScOption[Boolean] = ScNone
-//    var snapToCenterAlign: ScOption[Boolean] = ScNone
-//    var snapToEdgeAlign: ScOption[Boolean] = ScNone
-//    var snapToGrid: ScOption[Boolean] = ScNone
-//    var snapVDirection: ScOption[String] = ScNone
-//    var snapVGap: ScOption[Int] = ScNone
-//    var styleName: ScOption[CSSStyleName] = ScNone
-//    var tabIndex: ScOption[Int] = ScNone
-//    var top: ScOption[Int | String] = ScNone
-//    var topElement: ScOption[Canvas] = ScNone
-//    var useBackMask: ScOption[Boolean] = ScNone
-//    var useDragMask: ScOption[Boolean] = ScNone
-//    var useNativeDrag: ScOption[Boolean] = ScNone
-//    var useOpacityFilter: ScOption[Boolean] = ScNone
-//    var useTouchScrolling: ScOption[Boolean] = ScNone
-//    var valuesManager: ScOption[ValuesManager] = ScNone
-//    var visibility: ScOption[Visibility] = ScNone
-//    var visibilityChanged: ScOption[js.ThisFunction1[callbackHandler, Boolean, void]] = ScNone
-//    var width: ScOption[Int | String] = ScNone
+    var dropTypes: ScOption[Either[js.Array[String], String]] = ScNone
+    var dynamicContents: ScOption[Boolean] = ScNone
+    var dynamicContentsVars: ScOption[ValueMap] = ScNone
+    var edgeBackgroundColor: ScOption[Color] = ScNone
+    var edgeCenterBackgroundColor: ScOption[Color] = ScNone
+    var edgeCursorMap: ScOption[Types.Object] = ScNone
+    var edgeImage: ScOption[SCImgURL] = ScNone
+    var edgeMarginSize: ScOption[Int] = ScNone
+    var edgeOffset: ScOption[Int] = ScNone
+    var edgeOpacity: ScOption[Int] = ScNone
+    var edgeShowCenter: ScOption[Boolean] = ScNone
+    var edgeSize: ScOption[Int] = ScNone
+    var editNode: ScOption[EditNode] = ScNone
+    var editProxy: ScOption[EditNode] = ScNone
+    var editProxyConstructor: ScOption[SCClassName] = ScNone
+    var extraSpace: ScOption[Int] = ScNone
+    var focusChanged: ScOption[js.ThisFunction1[callbackHandler, Boolean, void]] = ScNone
+    var groupBorderCSS: ScOption[String] = ScNone
+    var groupLabelBackgroundColor: ScOption[CSSColor] = ScNone
+    var groupLabelStyleName: ScOption[CSSStyleName] = ScNone
+    var groupTitle: ScOption[HTMLString] = ScNone
+    var height: ScOption[Either[String, Int]] = ScNone
+    var hideUsingDisplayNone: ScOption[Boolean] = ScNone
+    var hover: ScOption[js.ThisFunction0[callbackHandler, Boolean]] = ScNone
+    var hoverAlign: ScOption[Alignment] = ScNone
+    var hoverAutoDestroy: ScOption[Boolean] = ScNone
+    var hoverDelay: ScOption[Int] = ScNone
+    var hoverHeight: ScOption[Int] = ScNone
+    var hoverMoveWithMouse: ScOption[Boolean] = ScNone
+    var hoverOpacity: ScOption[Int] = ScNone
+    var hoverStyle: ScOption[CSSStyleName] = ScNone
+    var hoverVAlign: ScOption[VerticalAlignment] = ScNone
+    var hoverWidth: ScOption[Int] = ScNone
+    var hoverWrap: ScOption[Boolean] = ScNone
+    var htmlElement: ScOption[HTMLElement] = ScNone
+    var htmlPosition: ScOption[DrawPosition] = ScNone
+    var ID: ScOption[String] = ScNone
+    var initWidget: ScOption[js.ThisFunction0[callbackHandler, void]] = ScNone
+    var isGroup: ScOption[Boolean] = ScNone
+    var isPrinting: ScOption[Boolean] = ScNone
+    var isSnapAlignCandidate: ScOption[Boolean] = ScNone
+    var keepInParentRect: ScOption[Either[Boolean, js.Array[Int]]] = ScNone
+    var keyDown: ScOption[js.ThisFunction0[callbackHandler, Boolean]] = ScNone
+    var keyPress: ScOption[js.ThisFunction0[callbackHandler, Boolean]] = ScNone
+    var keyUp: ScOption[js.ThisFunction0[callbackHandler, Boolean]] = ScNone
+    var layoutAlign: ScOption[Either[Alignment, VerticalAlignment]] = ScNone
+    var leavePageSpace: ScOption[Int] = ScNone
+    var left: ScOption[Either[String, Int]] = ScNone
+    var locateChildrenBy: ScOption[LocatorStrategy] = ScNone
+    var locateChildrenType: ScOption[LocatorTypeStrategy] = ScNone
+    var locatePeersBy: ScOption[LocatorStrategy] = ScNone
+    var locatePeersType: ScOption[LocatorTypeStrategy] = ScNone
+    var margin: ScOption[Int] = ScNone
+    var masterElement: ScOption[Canvas] = ScNone
+    var matchElement: ScOption[Boolean] = ScNone
+    var maxHeight: ScOption[Int] = ScNone
+    //    var maxWidth: ScOption[Int] = ScNone
+    //    var menuConstructor: ScOption[SCClassName] = ScNone
+    //    var minHeight: ScOption[Int] = ScNone
+    //    var minNonEdgeSize: ScOption[Int] = ScNone
+    //    var minWidth: ScOption[Int] = ScNone
+    //    var momentumScrollMinSpeed: ScOption[Int] = ScNone
+    //    var mouseDown: ScOption[js.ThisFunction0[callbackHandler, Boolean]] = ScNone
+    //    var mouseMove: ScOption[js.ThisFunction0[callbackHandler, Boolean]] = ScNone
+    //    var mouseOut: ScOption[js.ThisFunction0[callbackHandler, Boolean]] = ScNone
+    //    var mouseOver: ScOption[js.ThisFunction0[callbackHandler, Boolean]] = ScNone
+    //    var mouseStillDown: ScOption[js.ThisFunction0[callbackHandler, Boolean]] = ScNone
+    //    var mouseStillDownDelay: ScOption[Int] = ScNone
+    //    var mouseStillDownInitialDelay: ScOption[Int] = ScNone
+    //    var mouseUp: ScOption[js.ThisFunction0[callbackHandler, Boolean]] = ScNone
+    //    var mouseWheel: ScOption[js.ThisFunction0[callbackHandler, Boolean]] = ScNone
+    //    var moved: ScOption[js.ThisFunction2[callbackHandler, Int, Int, void]] = ScNone
+    //    var noDoubleClicks: ScOption[Boolean] = ScNone
+    //    var opacity: ScOption[Int] = ScNone
+    //    var overflow: ScOption[Overflow] = ScNone
+    //    var padding: ScOption[Int] = ScNone
+    //    var parentCanvas: ScOption[Canvas] = ScNone
+    //    var parentMoved: ScOption[js.ThisFunction2[callbackHandler, Int, Int, void]] = ScNone
+    //    var peers: ScOption[js.Array[Canvas]] = ScNone
+    //    var percentBox: ScOption[PercentBoxModel] = ScNone
+    //    var percentSource: ScOption[Canvas] = ScNone
+    //    var position: ScOption[Positioning] = ScNone
+    //    var printChildrenAbsolutelyPositioned: ScOption[Boolean] = ScNone
+    //    var prompt: ScOption[HTMLString] = ScNone
+    //    var proportionalResizeModifiers: ScOption[js.Array[KeyName]] = ScNone
+    //    var proportionalResizing: ScOption[ProportionalResizeMode] = ScNone
+    //    var redrawOnResize: ScOption[Boolean] = ScNone
+    //    var resizeBarTarget: ScOption[String] = ScNone
+    //    var resized: ScOption[js.ThisFunction0[callbackHandler, Boolean]] = ScNone
+    //    var resizeFrom: ScOption[js.Array[EdgeName]] = ScNone
+    //    var rightMouseDown: ScOption[js.ThisFunction0[callbackHandler, Boolean]] = ScNone
+    //    var scrollbarConstructor: ScOption[String] = ScNone
+    //    var scrollbarSize: ScOption[Int] = ScNone
+    //    var scrolled: ScOption[js.ThisFunction0[callbackHandler, Boolean]] = ScNone
+    //    var setDragTracker: ScOption[js.ThisFunction0[callbackHandler, Boolean]] = ScNone
+    //    var shadowDepth: ScOption[Int] = ScNone
+    //    var shadowImage: ScOption[Int] = ScNone
+    //    var shadowOffset: ScOption[Int] = ScNone
+    //    var shadowSoftness: ScOption[Int] = ScNone
+    //    var shouldPrint: ScOption[Boolean] = ScNone
+    //    var showCustomScrollbars: ScOption[Boolean] = ScNone
+    //    var showDragShadow: ScOption[Boolean] = ScNone
+    //    var showEdges: ScOption[Boolean] = ScNone
+    //    var showHover: ScOption[Boolean] = ScNone
+    //    var showHoverComponents: ScOption[Boolean] = ScNone
+    //    var showResizeBar: ScOption[Boolean] = ScNone
+    //    var showShadow: ScOption[Boolean] = ScNone
+    //    var showSnapGrid: ScOption[Boolean] = ScNone
+    //    var shrinkElementOnHide: ScOption[Boolean] = ScNone
+    //    var skinImgDir: ScOption[URL] = ScNone
+    //    var snapAlignCandidates: ScOption[js.Array[Canvas]] = ScNone
+    //    var snapAlignCenterLineStyle: ScOption[String] = ScNone
+    //    var snapAlignEdgeLineStyle: ScOption[String] = ScNone
+    //    var snapAxis: ScOption[String] = ScNone
+    //    var snapEdge: ScOption[String] = ScNone
+    //    var snapHDirection: ScOption[String] = ScNone
+    //    var snapHGap: ScOption[Int] = ScNone
+    //    var snapOffsetLeft: ScOption[Int] = ScNone
+    //    var snapOffsetTop: ScOption[Int] = ScNone
+    //    var snapOnDrop: ScOption[Int] = ScNone
+    //    var snapResizeToAlign: ScOption[Int] = ScNone
+    //    var snapResizeToGrid: ScOption[Int] = ScNone
+    //    var snapTo: ScOption[String] = ScNone
+    //    var snapToAlign: ScOption[Boolean] = ScNone
+    //    var snapToCenterAlign: ScOption[Boolean] = ScNone
+    //    var snapToEdgeAlign: ScOption[Boolean] = ScNone
+    //    var snapToGrid: ScOption[Boolean] = ScNone
+    //    var snapVDirection: ScOption[String] = ScNone
+    //    var snapVGap: ScOption[Int] = ScNone
+    //    var styleName: ScOption[CSSStyleName] = ScNone
+    //    var tabIndex: ScOption[Int] = ScNone
+    //    var top: ScOption[Int | String] = ScNone
+    //    var topElement: ScOption[Canvas] = ScNone
+    //    var useBackMask: ScOption[Boolean] = ScNone
+    //    var useDragMask: ScOption[Boolean] = ScNone
+    //    var useNativeDrag: ScOption[Boolean] = ScNone
+    //    var useOpacityFilter: ScOption[Boolean] = ScNone
+    //    var useTouchScrolling: ScOption[Boolean] = ScNone
+    //    var valuesManager: ScOption[ValuesManager] = ScNone
+    //    var visibility: ScOption[Visibility] = ScNone
+    //    var visibilityChanged: ScOption[js.ThisFunction1[callbackHandler, Boolean, void]] = ScNone
+    //    var width: ScOption[Int | String] = ScNone
 }
