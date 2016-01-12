501d97c53021b47f709befe628098184d70d74ea
andrew <uandrew1965@gmail.com>
2016-01-12 18:35:14 +0300

    Доработки

--- project/CommonTypesProj.scala
+++ project/CommonTypesProj.scala
@@ -1,33 +1,34 @@
 package com.simplesys.build
 
 import org.scalajs.sbtplugin.ScalaJSPlugin.AutoImport._
+import sbt.Keys._
 import sbt._
-import Keys._
 
 trait CommonTypesProj {
-  self: Build =>
+    self: Build =>
 
-  lazy val commonTypesCrossProj = crossProject.
-    settings(
-      name := "common-types",
-      version := "1.0-SNAPSHOT",
-      libraryDependencies ++= Seq(
-        CommonSettings.cmnDependencies.uTest.value,
-        CommonSettings.cmnDependencies.common.value,
-        CommonSettings.cmnDependencies.logbackWrapper.value
-      ),
-      testFrameworks += new TestFramework("utest.runner.Framework")
-    ).
-    jvmSettings(
-      libraryDependencies ++= {
-        Seq(
-        )
-      }).
-    jsSettings(
-      //jsDependencies += "org.example" %% "js-thing" % "0.1" / "foo.js"
-    )
+    lazy val commonTypesCrossProj = crossProject.
+      settings(
+          name := "common-types",
+          version := "1.0-SNAPSHOT",
+          libraryDependencies ++= Seq(
+              CommonSettings.cmnDependencies.uTest.value,
+              CommonSettings.cmnDependencies.xmlExtender.value,
+              CommonSettings.cmnDependencies.common.value,
+              CommonSettings.cmnDependencies.logbackWrapper.value
+          ),
+          testFrameworks += new TestFramework("utest.runner.Framework")
+      ).
+      jvmSettings(
+          libraryDependencies ++= {
+              Seq(
+              )
+          }).
+      jsSettings(
+          //jsDependencies += "org.example" %% "js-thing" % "0.1" / "foo.js"
+      )
 
-  // Needed, so sbt finds the projects
-  lazy val commonTypesJVM = commonTypesCrossProj.jvm
-  lazy val commonTypesJS = commonTypesCrossProj.js
+    // Needed, so sbt finds the projects
+    lazy val commonTypesJVM = commonTypesCrossProj.jvm
+    lazy val commonTypesJS = commonTypesCrossProj.js
 }
