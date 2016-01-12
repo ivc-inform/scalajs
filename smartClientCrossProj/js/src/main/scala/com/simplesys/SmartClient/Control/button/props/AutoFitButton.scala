8ed49f1a9312c721af877b32de04ec5d073b5d31
andrew <uandrew1965@gmail.com>
2016-01-12 21:17:55 +0300

    Доработки

---
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Control/button/props/AutoFitButton.scala
@@ -0,0 +1,8 @@
+package com.simplesys.SmartClient.Control.button.props
+
+import com.simplesys.SmartClient.Control.props.ButtonProps
+
+import scala.scalajs.js._
+
+class AutoFitButtonProps extends ButtonProps {
+}
