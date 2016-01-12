e1a2d3edb4b94c605fbca9fc4caea5878e34d796
andrew <uandrew1965@gmail.com>
2016-01-12 21:36:55 +0300

    Доработки

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Control/menu/MenuItem.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Control/menu/MenuItem.scala
@@ -1,12 +1,33 @@
 package com.simplesys.SmartClient.Control.menu
 
+import com.simplesys.SmartClient.Control.Menu
+import com.simplesys.SmartClient.Foundation.Canvas
 import com.simplesys.SmartClient.Grids.listGrid.ListGridRecord
+import com.simplesys.SmartClient.System.KeyIdentifier
+import com.simplesys.System.Types.{HTMLString, SCImgURL, void}
 
 import scala.scalajs.js
-import scala.scalajs.js.|
+import scala.scalajs.js.Array
 
 @js.native
 trait MenuItem extends ListGridRecord {
+    var action: js.Function1[void, _]
+    var canSelectParent: Boolean
+    var checked: Boolean
+    var checkIf: js.Function3[Canvas, Menu, MenuItem, Boolean]
+    var click: js.Function4[Canvas, MenuItem, Menu, Int, _]
+    var disabledIcon: String
+    var dynamicIcon: js.Function3[Canvas, Menu, MenuItem, SCImgURL]
+    var dynamicTitle: js.Function3[Canvas, Menu, MenuItem, String]
+    var enableIf: js.Function3[Canvas, Menu, MenuItem, Boolean]
+    var fetchSubmenus: Boolean
+    val icon: String
+    val iconHeight: Int
+    val iconWidth: Int
+    val keys: Array[KeyIdentifier]
+    val keyTitle: String
+    val submenu: Menu
+    val title: HTMLString
 }
 
 
