d2c95ea8d71f2f40a699e10f8ebb9fb82208167b
andrew <uandrew1965@gmail.com>
2016-01-12 22:08:01 +0300

    Доработки

---
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Layout/props/HLayoutProps.scala
@@ -0,0 +1,6 @@
+package com.simplesys.SmartClient.Layout.props
+
+import scala.scalajs.js._
+
+class HLayoutProps extends LayoutProps {
+}
--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Layout/props/LayoutProps.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Layout/props/LayoutProps.scala
@@ -1,7 +1,53 @@
 package com.simplesys.SmartClient.Layout.props
 
+import com.simplesys.SmartClient.Foundation.Canvas
 import com.simplesys.SmartClient.Foundation.props.CanvasProps
+import com.simplesys.SmartClient.Layout.Splitbar
+import com.simplesys.System.Types.Alignment.Alignment
+import com.simplesys.System.Types.LayoutPolicy.LayoutPolicy
+import com.simplesys.System.Types.LayoutResizeBarPolicy.LayoutResizeBarPolicy
+import com.simplesys.System.Types.LocatorStrategy.LocatorStrategy
+import com.simplesys.System.Types.LocatorTypeStrategy.LocatorTypeStrategy
+import com.simplesys.System.Types.VerticalAlignment.VerticalAlignment
+import com.simplesys.System.Types.{MultiAutoChild, void}
+import com.simplesys.option.{DoubleAlignment, ScNone, ScOption}
 
-class LayoutProps extends CanvasProps{
+import scala.scalajs.js
 
+class LayoutProps extends CanvasProps {
+    var align: ScOption[DoubleAlignment[Alignment, VerticalAlignment]] = ScNone
+    var animateMembers: ScOption[Boolean] = ScNone
+    var animateMemberTime: ScOption[Int] = ScNone
+    var canDropComponents: ScOption[Boolean] = ScNone
+    var defaultLayoutAlign: ScOption[DoubleAlignment[Alignment, VerticalAlignment]] = ScNone
+    var defaultResizeBars: ScOption[LayoutResizeBarPolicy] = ScNone
+    var dropLine: ScOption[Canvas] = ScNone
+    var dropLineThickness: ScOption[Int] = ScNone
+    var enforcePolicy: ScOption[Boolean] = ScNone
+    var hPolicy: ScOption[LayoutPolicy] = ScNone
+    var layoutBottomMargin: ScOption[Int] = ScNone
+    var layoutLeftMargin: ScOption[Int] = ScNone
+    var layoutMargin: ScOption[Int] = ScNone
+    var layoutRightMargin: ScOption[Int] = ScNone
+    var layoutTopMargin: ScOption[Int] = ScNone
+    var leaveScrollbarGap: ScOption[LocatorStrategy] = ScNone
+    var locateMembersType: ScOption[LocatorTypeStrategy] = ScNone
+    var managePercentBreadth: ScOption[Boolean] = ScNone
+    var memberOverlap: ScOption[Int] = ScNone
+    var members: ScOption[js.Array[Canvas]] = ScNone
+    var membersChanged: ScOption[js.ThisFunction0[callbackHandler, void]] = ScNone
+    var membersMargin: ScOption[Int] = ScNone
+    var minMemberSize: ScOption[Int] = ScNone
+    var paddingAsLayoutMargin: ScOption[Boolean] = ScNone
+    var placeHolderDefaults: ScOption[Canvas] = ScNone
+    var placeHolderProperties: ScOption[Canvas] = ScNone
+    var resizeBar: ScOption[Splitbar with MultiAutoChild] = ScNone
+    var resizeBarClass: ScOption[String] = ScNone
+    var resizeBarSize: ScOption[Int] = ScNone
+    var reverseOrder: ScOption[Boolean] = ScNone
+    var showDragPlaceHolder: ScOption[Boolean] = ScNone
+    var showDropLines: ScOption[Boolean] = ScNone
+    var stackZIndex: ScOption[String] = ScNone
+    var vertical: ScOption[Boolean] = ScNone
+    var vPolicy: ScOption[LayoutPolicy] = ScNone
 }
