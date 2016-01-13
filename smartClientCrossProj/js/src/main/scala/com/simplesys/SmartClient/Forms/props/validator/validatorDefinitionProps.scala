84af1726e312bf31dea52f9218dce8193324cef1
andrew <uandrew1965@gmail.com>
2016-01-13 18:54:36 +0300

    Доработки

---
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Forms/props/validator/validatorDefinitionProps.scala
@@ -0,0 +1,8 @@
+package com.simplesys.SmartClient.Forms.props.validator
+
+import com.simplesys.props.AbstractClassProps
+
+import scala.scalajs.js._
+
+class validatorDefinitionProps extends AbstractClassProps {
+}
