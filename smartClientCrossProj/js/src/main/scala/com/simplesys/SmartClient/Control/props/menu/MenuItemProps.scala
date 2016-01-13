267c690cfa91372c2b6a164bf938cb998286b0dc
andrew <uandrew1965@gmail.com>
2016-01-13 21:32:33 +0300

    Доработки

---
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Control/props/menu/MenuItemProps.scala
@@ -0,0 +1,8 @@
+package com.simplesys.SmartClient.Control.props.menu
+
+import com.simplesys.SmartClient.Grids.props.listGrid.ListGridRecordProps
+
+import scala.scalajs.js._
+
+class MenuItemProps extends ListGridRecordProps {
+}
