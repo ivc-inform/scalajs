a57d761655cc39a8511be9ce834b74c4e293a465
andrew <uandrew1965@gmail.com>
2016-01-13 17:14:13 +0300

    Доработки

---
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/DataBinding/props/dataSource/FileSpecProps.scala
@@ -0,0 +1,8 @@
+package com.simplesys.SmartClient.DataBinding.props.dataSource
+
+import com.simplesys.props.AbstractClassProps
+
+import scala.scalajs.js._
+
+class FileSpecProps extends AbstractClassProps {
+}
---
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/DataBinding/props/dataSource/SerializationContextProps.scala
@@ -0,0 +1,8 @@
+package com.simplesys.SmartClient.DataBinding.props.dataSource
+
+import com.simplesys.props.AbstractClassProps
+
+import scala.scalajs.js._
+
+class SerializationContextProps extends AbstractClassProps {
+}
