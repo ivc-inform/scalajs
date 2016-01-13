a1eb246765644862c6612dce415cbea106e3acc1
andrew <uandrew1965@gmail.com>
2016-01-13 17:41:55 +0300

    Доработки

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Forms/FormsItems/BlurbItem.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Forms/FormsItems/BlurbItem.scala
@@ -3,24 +3,23 @@ package com.simplesys.SmartClient.Forms.FormsItems
 import com.simplesys.System.Types.SCClassName
 
 import scala.scalajs.js
-import scala.scalajs.js.|
+import scala.scalajs.js.annotation.JSName
 
 @js.native
 trait BlurbItem extends FormItem {
-    var clipValue:Boolean
-    var editProxyConstructor:SCClassName
-    var wrap:Boolean
+    var clipValue: Boolean
+    var editProxyConstructor: SCClassName
+    var wrap: Boolean
 }
 
 @js.native
 abstract trait AbstractBlurbItemCompanion extends AbstractFormItemCompanion {
 }
 
-package companion {
-
 import scala.scalajs.js
 
 @js.native
-   object BlurbItem extends AbstractBlurbItemCompanion
-}
+@JSName("BlurbItem")
+object BlurbItemStatic extends AbstractBlurbItemCompanion
+
 
