6771004b928badc973e21f7718d5772fd22b0c21
andrew <uandrew1965@gmail.com>
2016-01-13 17:19:48 +0300

    Доработки

---
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/DataBinding/props/AdvancedCriteriaProps.scala
@@ -0,0 +1,6 @@
+package com.simplesys.SmartClient.DataBinding.props
+
+import scala.scalajs.js._
+
+class AdvancedCriteriaProps extends CriterionProps {
+}
---
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/DataBinding/props/DataBoundComponentProps.scala
@@ -0,0 +1,6 @@
+package com.simplesys.SmartClient.DataBinding.props
+
+import com.simplesys.SmartClient.System.props.ClassProps
+
+trait DataBoundComponentProps extends ClassProps {
+}
