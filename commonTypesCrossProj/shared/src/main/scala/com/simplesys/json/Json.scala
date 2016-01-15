acae7fb149d958af54a372fd97c0d33ffd46f815
andrew <uandrew1965@gmail.com>
2016-01-15 23:08:03 +0300

    Доработки

--- commonTypesCrossProj/shared/src/main/scala/com/simplesys/json/Json.scala
+++ commonTypesCrossProj/shared/src/main/scala/com/simplesys/json/Json.scala
@@ -1,40 +1,18 @@
 package com.simplesys.json
 
-import com.simplesys.common.Strings._
+//import com.simplesys.common.Strings._
 
 import scala.collection.GenSeq
 
-object Json {
+object Json extends {
     type funcFormatter = Any => String
 
     private val defaultFormatter: funcFormatter = (in: Any) => {
         in match {
-            case s: String => s.dblQuoted
-            case jsonObject: Map[String, Any] => jsonObject toJsonString (defaultFormatter)
-            case jsonList: GenSeq[Any] => jsonList toJsonString (defaultFormatter)
-            case function: Function0[_] => "$function0$"
-            case function: Function1[_, _] => "$function1$"
-            case function: Function2[_, _, _] => "$function2$"
-            case function: Function3[_, _, _, _] => "$function3$"
-            case function: Function4[_, _, _, _, _] => "$function4$"
-            case function: Function5[_, _, _, _, _, _] => "$function5$"
-            case function: Function6[_, _, _, _, _, _, _] => "$function6$"
-            case function: Function7[_, _, _, _, _, _, _, _] => "$function7$"
-            case function: Function8[_, _, _, _, _, _, _, _, _] => "$function8$"
-            case function: Function9[_, _, _, _, _, _, _, _, _, _] => "$function9$"
-            case function: Function10[_, _, _, _, _, _, _, _, _, _, _] => "$function10$"
-            case function: Function11[_, _, _, _, _, _, _, _, _, _, _, _] => "$function11$"
-            case function: Function12[_, _, _, _, _, _, _, _, _, _, _, _, _] => "$function12$"
-            case function: Function13[_, _, _, _, _, _, _, _, _, _, _, _, _, _] => "$function13$"
-            case function: Function14[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _] => "$function14$"
-            case function: Function15[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _] => "$function15$"
-            case function: Function16[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _] => "$function16$"
-            case function: Function17[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _] => "$function17$"
-            case function: Function18[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _] => "$function18$"
-            case function: Function19[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _] => "$function19$"
-            case function: Function20[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _] => "$function20$"
-            case function: Function21[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _] => "$function21$"
-            case function: Function22[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _] => "$function22$"
+            case s: String => s /*.dblQuoted*/
+            case jsonObject: Map[String, Any] => jsonObject toJsonString()
+            case jsonList: GenSeq[Any] => jsonList toJsonString()
+            case other if other.toString.indexOf("function") > -1 => "undefined"
             case other =>
                 other match {
                     case null =>
@@ -46,13 +24,12 @@ object Json {
     }
 
     implicit class map2JsonString(map: Map[String, Any]) {
-        def toJsonString(formatter: funcFormatter) = "{" + map.map {
-            case (key, value) => key.dblQuoted + ":" + formatter(value)
+        def toJsonString(formatter: funcFormatter = defaultFormatter) = "{" + map.map {
+            case (key, value) => "\"" + key + "\"" /*.dblQuoted*/ + ":" + formatter(value)
         }.mkString(",") + "}"
     }
 
     implicit class seq2JsonString(map: GenSeq[Any]) {
-        def toJsonString(formatter: funcFormatter) = "[" + map.map(formatter).mkString(s",") + "]"
+        def toJsonString(formatter: funcFormatter = defaultFormatter) = "[" + map.map(formatter).mkString(s",") + "]"
     }
-
 }
--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/System/SCApply.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/System/SCApply.scala
@@ -1,17 +1,16 @@
 package com.simplesys.SmartClient.System
 
-import com.simplesys.props.AbstractClassProps
 import com.simplesys.macros.PropsToDictionary
+import com.simplesys.props.AbstractClassProps
+import com.simplesys.json.Json._
 
 import scala.reflect.ClassTag
 import scala.scalajs.js
-import scala.scalajs.js.{Any, Dictionary}
-import com.simplesys.json.Json._
 
 class SCApply[T <: Class, P <: AbstractClassProps](implicit classTag_T: ClassTag[T], propsToDictionary: PropsToDictionary[P]) {
     def create(propsClass: P): T = js.Dynamic.global.isc.selectDynamic(classTag_T.runtimeClass.getSimpleName).create(propsToDictionary.getDictionary(propsClass)).asInstanceOf[T]
-    def properties(propsClass: P):T = propsToDictionary.getDictionary(propsClass).asInstanceOf[T]
-    def jsonString(propsClass: P):String = propsToDictionary.getMap(propsClass).toJsonString()
+    def properties(propsClass: P): T = propsToDictionary.getDictionary(propsClass).asInstanceOf[T]
+    def jsonString(propsClass: P): String = propsToDictionary.getMap(propsClass).toMap.toJsonString()
 }
 
 
