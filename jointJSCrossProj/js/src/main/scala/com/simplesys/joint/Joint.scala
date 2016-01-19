feffb8b73941c277a15f8e8543b4df2fc1d7cbfe
andrew <uandrew1965@gmail.com>
2016-01-19 07:22:10 +0300

    Допвботки

---
+++ jointJSCrossProj/js/src/main/scala/com/simplesys/joint/Joint.scala
@@ -0,0 +1,16 @@
+package com.simplesys.joint
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSName
+
+@js.native
+trait Joint extends js.Object {
+
+}
+
+
+@js.native
+@JSName("joint")
+object JointStatic extends js.Object {
+    val joint: Joint = js.native
+}
