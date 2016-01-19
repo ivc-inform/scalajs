5c0670070da3d1470dc30a2412d20af140fb996b
andrew <uandrew1965@gmail.com>
2016-01-19 20:51:12 +0300

    Доработки

---
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Layout/props/tabSet/Tab.scala
@@ -0,0 +1,9 @@
+package com.simplesys.SmartClient.Layout.props.tabSet
+
+import com.simplesys.SmartClient.System.Types
+import com.simplesys.props.AbstractClassProps
+
+import scala.scalajs.js._
+
+class TabProps extends AbstractClassProps {
+}
--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Layout/tabSet/Tab.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Layout/tabSet/Tab.scala
@@ -3,32 +3,31 @@ package com.simplesys.SmartClient.Layout.tabSet
 import com.simplesys.SmartClient.Foundation.Canvas
 import com.simplesys.SmartClient.Layout.TabSet
 import com.simplesys.SmartClient.System.Types
-import com.simplesys.System.Types.{HTMLString, SCImgURL, void}
+import com.simplesys.System.Types.{void, HTMLString, SCImgURL}
 
 import scala.scalajs.js
-import scala.scalajs.js.annotation.JSName
 import scala.scalajs.js.|
 
 @js.native
 trait Tab extends Types.Object {
-//    var canClose: Boolean
-//    var canEditTitle: Boolean
-//    var canReorder: Boolean
-//    var closeIcon: SCImgURL
-//    var closeIconSize: Int
-//    var disabled: Boolean
-//    var icon: SCImgURL
-//    var iconHeight: Int
-//    var iconSize: Int
-//    var iconWidth: Int
-//    var ID: String
-//    var name: String
-//    var pane: String | Canvas
-//    var paneMargin: Int
-//    var pickerTitle: HTMLString
-//    var prompt: HTMLString
-//    var tabDeselected: js.Function7[TabSet, Int, Canvas, String, Tab, Tab, String, Boolean]
-//    var tabSelected: js.Function6[TabSet, Int, Canvas, String, Tab, String, void]
-    val title: HTMLString
-//    var width: Int
+    var canClose: Boolean
+    var canEditTitle: Boolean
+    var canReorder: Boolean
+    var closeIcon: SCImgURL
+    var closeIconSize: Int
+    var disabled: Boolean
+    var icon: SCImgURL
+    var iconHeight: Int
+    var iconSize: Int
+    var iconWidth: Int
+    var ID: String
+    var name: String
+    var pane: String | Canvas
+    var paneMargin: Int
+    var pickerTitle: HTMLString
+    var prompt: HTMLString
+    var tabDeselected: js.Function7[TabSet, Int, Canvas, String, Tab, Tab, String, Boolean]
+    var tabSelected: js.Function6[TabSet, Int, Canvas, String, Tab, String, void]
+    var title: HTMLString
+    var width: Int
 }
