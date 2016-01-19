ae93efa7249ed2109a206c76a2d8123d0bbadf08
andrew <uandrew1965@gmail.com>
2016-01-19 19:51:33 +0300

    Доработки

---
+++ smartClientCrossProj/js/src/main/resources/test.js
@@ -0,0 +1,12 @@
+isc.ListGrid.create(
+    {
+        ID                   : "countryList",
+        width                : 500,
+        height               : 224,
+        alternateRecordStyles: true,
+        showAllRecords       : true,
+        dataSource           : DataSource.create(
+        ),
+        autoFetchData        : true
+    }
+)
