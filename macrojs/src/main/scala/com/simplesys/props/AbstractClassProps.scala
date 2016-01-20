a3357ed4c7820d4cd1c358e032b848d8174270f3
andrew <uandrew1965@gmail.com>
2016-01-20 15:35:02 +0300

    Доработки

--- macrojs/src/main/scala/com/simplesys/props/AbstractClassProps.scala
+++ macrojs/src/main/scala/com/simplesys/props/AbstractClassProps.scala
@@ -1,3 +1,9 @@
 package com.simplesys.props
 
-abstract class AbstractClassProps
+import com.simplesys.macros.PropsToDictionary
+
+class AbstractClassProps extends Dict[AbstractClassProps]
+
+class Dict[P <: AbstractClassProps](implicit propsToDictionary: PropsToDictionary[P]) {
+    def create(propsClass: P) = propsToDictionary.getDictionary(propsClass)
+}
