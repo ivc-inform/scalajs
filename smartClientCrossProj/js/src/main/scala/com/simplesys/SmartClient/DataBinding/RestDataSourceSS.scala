aac8f32eed9cb22f406045be8325222d5cc45c56
andrew <uandrew1965@gmail.com>
2016-01-23 17:21:10 +0300

    Допвботки

---
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/DataBinding/RestDataSourceSS.scala
@@ -0,0 +1,10 @@
+package com.simplesys.SmartClient.DataBinding
+
+import scala.scalajs.js
+
+@js.native
+trait RestDataSourceSS extends RestDataSource
+
+@js.native
+object RestDataSourceSS extends AbstractDataSourceCompanion
+
