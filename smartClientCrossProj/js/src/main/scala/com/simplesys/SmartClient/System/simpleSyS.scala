4f4a968eca36df2ef020ca29e0b867faffafcd81
andrew <uandrew1965@gmail.com>
2016-01-26 16:54:22 +0300

    Доработки

---
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/System/simpleSyS.scala
@@ -0,0 +1,10 @@
+package com.simplesys.SmartClient.System
+
+import com.simplesys.SmartClient.Foundation.Canvas
+
+import scala.scalajs.js
+
+@js.native
+object simpleSyS extends Types.Object {
+    def checkOwner(canvas: Canvas): Canvas = js.native
+}
