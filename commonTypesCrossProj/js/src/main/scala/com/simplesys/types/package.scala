87022a10af5a38ce6920b71bed185509677f16df
andrew <uandrew1965@gmail.com>
2016-01-27 07:29:58 +0300

    Допвботки

--- commonTypesCrossProj/js/src/main/scala/com/simplesys/types/package.scala
+++ commonTypesCrossProj/js/src/main/scala/com/simplesys/types/package.scala
@@ -7,4 +7,5 @@ package object types {
     type JSArrayAny = js.Array[JSAny]
     type JSArray[T] = js.Array[T]
     type JSDictionaryAny = js.Dictionary[JSAny]
+    type JSDictionary[T] = js.Dictionary[T]
 }
