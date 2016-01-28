088acaa6557dd4a70ebb253a97c63b6d14a96b01
andrew <uandrew1965@gmail.com>
2016-01-28 17:49:49 +0300

    Доработки

---
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Grids/props/TreeListGridEditor.scala
@@ -0,0 +1,8 @@
+package com.simplesys.SmartClient.Grids.props
+
+import com.simplesys.SmartClient.Layout.props.HLayoutSSProps
+
+import scala.scalajs.js._
+
+class TreeListGridEditorProps extends HLayoutSSProps {
+}
