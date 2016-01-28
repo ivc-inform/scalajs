527c292d9193641cdb5b542f86f1ddb7fb3de9cd
andrew <uandrew1965@gmail.com>
2016-01-28 17:58:13 +0300

    Доработки

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Grids/props/TreeListGridEditor.scala
+++
@@ -1,8 +0,0 @@
-package com.simplesys.SmartClient.Grids.props
-
-import com.simplesys.SmartClient.Layout.props.HLayoutSSProps
-
-import scala.scalajs.js._
-
-class TreeListGridEditorProps extends HLayoutSSProps {
-}
---
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Grids/props/TreeListGridEditorProps.scala
@@ -0,0 +1,59 @@
+package com.simplesys.SmartClient.Grids.props
+
+import com.simplesys.SmartClient.Grids.listGrid.ListGridRecord
+import com.simplesys.SmartClient.Layout.props.HLayoutSSProps
+import com.simplesys.System.Types.SelectionStyle.SelectionStyle
+import com.simplesys.System.Types.TextMatchStyle.TextMatchStyle
+import com.simplesys.option.{IntString, ScOption, ScNone}
+import com.simplesys.types.JSArray
+
+import scala.scalajs.js._
+
+class TreeListGridEditorProps extends HLayoutSSProps {
+    var widthTree: ScOption[IntString[Int, String]] = ScNone
+    var widthList: ScOption[IntString[Int, String]] = ScNone
+    var fetchTreeDelay: ScOption[Int] = ScNone
+    var fetchListDelay: ScOption[Int] = ScNone
+    var showTreeRecordComponents: ScOption[Boolean] = ScNone
+    var showListRecordComponents: ScOption[Boolean] = ScNone
+    var editByCellTree: ScOption[Boolean] = ScNone
+    var editByCellList: ScOption[Boolean] = ScNone
+    var enableChangeSelectionTree: ScOption[Boolean] = ScNone
+    var showAllChild: ScOption[Boolean] = ScNone
+    var selectionTypeTree: ScOption[SelectionStyle] = ScNone
+    var selectionTypeList: ScOption[SelectionStyle] = ScNone
+    var getTreeSelection: ScOption[JSArray[ListGridRecord]] = ScNone
+    var showTreeFilterEditor: ScOption[Boolean] = ScNone
+    var canAcceptDroppedRecordsTree: ScOption[Boolean] = ScNone
+    var canAcceptDroppedRecordsList: ScOption[Boolean] = ScNone
+    var autoFetchData: ScOption[Boolean] = ScNone
+    var showTreeAdvancedFilter: ScOption[Boolean] = ScNone
+    var cancelEditingConfirmationMessageList: ScOption[String] = ScNone
+    var cancelEditingConfirmationMessageTree: ScOption[String] = ScNone
+    var showListAdvancedFilte: ScOption[Boolean] = ScNone
+    var canAcceptDropTree: ScOption[Boolean] = ScNone
+    var drawAheadRatioList: ScOption[Double] = ScNone
+    var drawAheadRatioTree: ScOption[Double] = ScNone
+    var showResizeBarList: ScOption[Boolean] = ScNone
+    var heightTree: ScOption[IntString[Int, String]] = ScNone
+    var heightList: ScOption[IntString[Int, String]] = ScNone
+    var canSelectCellsLis: ScOption[Boolean] = ScNone
+    var showTreeRecordComponentsByCell: ScOption[Boolean] = ScNone
+    var showListRecordComponentsByCell: ScOption[Boolean] = ScNone
+    var canSelectCellsTree: ScOption[Boolean] = ScNone
+    var emptyMessageTree: ScOption[String] = ScNone
+    var emptyMessageList: ScOption[String] = ScNone
+    var showResizeBarTree: ScOption[Boolean] = ScNone
+    var canReparentNodesTree: ScOption[Boolean] = ScNone
+    var showListFilterEditor: ScOption[Boolean] = ScNone
+    var filterTreeOnKeypress: ScOption[Boolean] = ScNone
+    var filterListOnKeypress: ScOption[Boolean] = ScNone
+    var showOpenIconsTree: ScOption[Boolean] = ScNone
+    var loadDataOnDemandTree: ScOption[Boolean] = ScNone
+    var dataPageSizeTree: ScOption[Int] = ScNone
+    var dataPageSizeList: ScOption[Int] = ScNone
+    var autoFetchTextMatchStyleTree: ScOption[TextMatchStyle] = ScNone
+    var autoFetchTextMatchStyleList: ScOption[TextMatchStyle] = ScNone
+    var canEditTree: ScOption[Boolean] = ScNone
+    var canEditList: ScOption[Boolean] = ScNone
+}
