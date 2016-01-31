28f8b2bb6ec942ae8e4763e10d45668188c908e3
andrew <uandrew1965@gmail.com>
2016-01-31 18:10:36 +0300

    Допвботки

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Grids/Grid.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Grids/Grid.scala
@@ -372,7 +372,7 @@ trait Grid[T <: ListGridField, R <: ListGridRecord] extends VLayout with DataBou
     def getEditedRecord(valuesID: Int | Types.Object): Types.Object
     def getEditField(): Types.Object
     def getEditFormItem(field: String | Int): FormItem
-    def getEditorProperties(field: T): Types.Object
+    var getEditorProperties: js.Function1[T,Types.Object]
     def getEditorType(field: T, values: Types.Object): String
     def getEditorValueIcons(field: T, values: Types.Object): Types.Object
     def getEditorValueMap(field: T, values: Types.Object): ValueMap
