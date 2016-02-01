83842f00c773076547e545992e21b94c4931ac20
andrew <uandrew1965@gmail.com>
2016-02-01 07:56:43 +0300

    Допвботки

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/DataBinding/Callbacks.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/DataBinding/Callbacks.scala
@@ -10,34 +10,34 @@ import scala.scalajs.js
 import scala.scalajs.js.{ThisFunction2, ThisFunction0, ThisFunction1}
 
 @js.native
-trait AnimationCallback extends ThisFunction1[Canvas#callbackHandler, Boolean, void]
+trait AnimationCallback extends ThisFunction1[Canvas#callbackHandler, Boolean, _]
 
 @js.native
-trait CollapseSectionCallback extends ThisFunction0[SectionStack#callbackHandler, void]
+trait CollapseSectionCallback extends ThisFunction0[SectionStack#callbackHandler, _]
 
 @js.native
-trait ExpandSectionCallback extends ThisFunction0[SectionStack#callbackHandler, void]
+trait ExpandSectionCallback extends ThisFunction0[SectionStack#callbackHandler, _]
 
 @js.native
-trait HideSectionCallback extends ThisFunction0[SectionStack#callbackHandler, void]
+trait HideSectionCallback extends ThisFunction0[SectionStack#callbackHandler, _]
 
 @js.native
-trait ShowSectionCallback extends ThisFunction0[SectionStack#callbackHandler, void]
+trait ShowSectionCallback extends ThisFunction0[SectionStack#callbackHandler, _]
 
 @js.native
-trait ClientOnlyDataSourceCallback extends ThisFunction0[DataSource, void]
+trait ClientOnlyDataSourceCallback extends ThisFunction0[DataSource, _]
 
 @js.native
-trait  GetFileCallback extends ThisFunction2[DSResponse, String, DSRequest, void]
+trait  GetFileCallback extends ThisFunction2[DSResponse, String, DSRequest, _]
 
 @js.native
-trait  GetFileVersionCallback extends ThisFunction2[DSResponse, String, DSRequest, void]
+trait  GetFileVersionCallback extends ThisFunction2[DSResponse, String, DSRequest, _]
 
 @js.native
-trait  HasFileCallback extends ThisFunction2[DSResponse, String, DSRequest, void]
+trait  HasFileCallback extends ThisFunction2[DSResponse, String, DSRequest, _]
 
 @js.native
-trait DSCallback extends js.Function3[DSResponse,JSDictionaryAny, DSRequest, void]
+trait DSCallback extends js.Function3[DSResponse,JSDictionaryAny, DSRequest, _]
 
 @js.native
-trait RPCCallback extends js.Function3[RPCResponse,JSDictionaryAny, RPCRequest, void]
+trait RPCCallback extends js.Function3[RPCResponse,JSDictionaryAny, RPCRequest, _]
