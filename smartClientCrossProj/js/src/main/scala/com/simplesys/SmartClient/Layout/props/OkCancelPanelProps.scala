7d79d4717b32821ea5454c4d4043ae8a3e75dba0
andrew <uandrew1965@gmail.com>
2016-02-01 16:08:36 +0300

    Доработки

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Layout/props/OkCancelPanelProps.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Layout/props/OkCancelPanelProps.scala
@@ -1,10 +1,12 @@
 package com.simplesys.SmartClient.Layout.props
 
+import com.simplesys.SmartClient.Layout.{OkCancelPanel, HLayout}
 import com.simplesys.option.{ScOption, ScNone}
 
 import scala.scalajs.js
 
 class OkCancelPanelProps extends HPanelSSProps {
-    var okFunction: ScOption[js.Function0[_]] = ScNone
-    var cancelFunction: ScOption[js.Function0[_]] = ScNone
+    type callbackHandler <: OkCancelPanel
+    var okFunction: ScOption[js.Function1[callbackHandler, _]] = ScNone
+    var cancelFunction: ScOption[js.Function1[callbackHandler, _]] = ScNone
 }
