2e95954446ce6720b89d6ae8db2d8701aea6a22b
andrew <uandrew1965@gmail.com>
2016-02-05 13:22:20 +0300

    Доработки

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Grids/listGrid/MasterDetailMapping.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Grids/listGrid/MasterDetailMapping.scala
@@ -7,10 +7,8 @@ import scala.scalajs.js.annotation.{JSName, ScalaJSDefined}
 import scala.scalajs.js.|
 
 @ScalaJSDefined
-trait PKForignMapping extends Types.Object {
-    @JSName ("pk")
+trait MasterDetailMapping extends Types.Object {
     val masterGridField: String
-    @JSName ("forign")
     val detailGridField: String
 }
 
