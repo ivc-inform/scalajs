3a8f4916a3d75306165cfb9e4a7abfff8984fb80
andrew <uandrew1965@gmail.com>
2016-02-07 08:49:27 +0300

    .

--- commonTypesCrossProj/js/src/main/scala/com/simplesys/option/package.scala
+++
@@ -1,7 +0,0 @@
-package com.simplesys
-
-import scala.scalajs.js
-
-package object option {
-    //implicit def Int2Any[T](x: Int): JSAny = x.asInstanceOf[JSAny]
-}
--- commonTypesCrossProj/shared/src/main/scala/com/simplesys/option/DoubleTypes.scala
+++ commonTypesCrossProj/shared/src/main/scala/com/simplesys/option/DoubleTypes.scala
@@ -3,7 +3,9 @@ package com.simplesys.option
 import com.simplesys.System.Types.Alignment._
 import com.simplesys.System.Types.FormItemType.FormItemType
 import com.simplesys.System.Types.VerticalAlignment._
+import com.simplesys.System._
 import scala.language.existentials
+import scala.scalajs.js
 
 sealed abstract class DoubleType[+A, +B]
 
@@ -44,6 +46,30 @@ case class StringfromFormItemType_String(a: String) extends FormItemType_String[
 object FormItemType_String {
     implicit def FormItemType2FormItemType_String(x: FormItemType) = ScSome(FormItemTypefromFormItemType_String(x))
     implicit def String2FormItemType_String(x: String) = ScSome(StringfromFormItemType_String(x))
+}
+//</editor-fold>
+
+//<editor-fold desc="Function1CanvasClick_String">
+sealed abstract class Function1CanvasClick_String[+A, +B]
+
+case class Function1CanvasClickfromFunction1CanvasClick_String(a: js.ThisFunction0[_, _]) extends Function1CanvasClick_String[js.ThisFunction0[_, _], String]
+case class StringfromFunction1CanvasClick_String(a: String) extends Function1CanvasClick_String[js.ThisFunction0[_, _], String]
+
+object Function1CanvasClick_String {
+    implicit def Function1CanvasClick2Function1CanvasClick_String(x: js.ThisFunction0[_, _]) = ScSome(Function1CanvasClickfromFunction1CanvasClick_String(x))
+    implicit def String2Function1CanvasClick_String(x: String) = ScSome(StringfromFunction1CanvasClick_String(x))
+}
+//</editor-fold>
+
+//<editor-fold desc="ArrayAny_DictAny">
+sealed abstract class ArrayAny_DictAny[+A, +B]
+
+case class ArrayAnyfromArrayAny_DictAny(a: JSArrayAny) extends ArrayAny_DictAny[JSArrayAny, JSDictionaryAny]
+case class DictfromArrayAny_DictAny(a: JSDictionaryAny) extends ArrayAny_DictAny[JSArrayAny, JSDictionaryAny]
+
+object ArrayAny_DictAny {
+    implicit def ArrayAny2ArrayAny_DictAny(x: JSArrayAny) = ScSome(ArrayAnyfromArrayAny_DictAny(x))
+    implicit def Dict2ArrayAny_DictAny(x: JSDictionaryAny) = ScSome(DictfromArrayAny_DictAny(x))
 }
 //</editor-fold>
 
