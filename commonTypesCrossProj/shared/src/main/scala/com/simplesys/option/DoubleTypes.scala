ee9be47adea969c607a381297d1ee04a6cfa9dec
andrew <uandrew1965@gmail.com>
2016-02-26 16:57:03 +0300

    Доработки

--- commonTypesCrossProj/shared/src/main/scala/com/simplesys/option/DoubleTypes.scala
+++ commonTypesCrossProj/shared/src/main/scala/com/simplesys/option/DoubleTypes.scala
@@ -50,14 +50,14 @@ object FormItemType_String {
 //</editor-fold>
 
 //<editor-fold desc="Function1CanvasClick_String">
-sealed abstract class Function1CanvasClick_String[+A, +B]
+sealed abstract class Function0CanvasClick_String[+A, +B]
 
-case class Function1CanvasClickfromFunction1CanvasClick_String(a: js.ThisFunction0[_, _]) extends Function1CanvasClick_String[js.ThisFunction0[_, _], String]
-case class StringfromFunction1CanvasClick_String(a: String) extends Function1CanvasClick_String[js.ThisFunction0[_, _], String]
+case class Function0CanvasClickfromFunction0CanvasClick_String(a: js.ThisFunction0[_, _]) extends Function0CanvasClick_String[js.ThisFunction0[_, _], String]
+case class StringfromFunction0CanvasClick_String(a: String) extends Function0CanvasClick_String[js.ThisFunction0[_, _], String]
 
-object Function1CanvasClick_String {
-    implicit def Function1CanvasClick2Function1CanvasClick_String(x: js.ThisFunction0[_, _]) = ScSome(Function1CanvasClickfromFunction1CanvasClick_String(x))
-    implicit def String2Function1CanvasClick_String(x: String) = ScSome(StringfromFunction1CanvasClick_String(x))
+object Function0CanvasClick_String {
+    implicit def Function1CanvasClick2Function1CanvasClick_String(x: js.ThisFunction0[_, _]) = ScSome(Function0CanvasClickfromFunction0CanvasClick_String(x))
+    implicit def String2Function1CanvasClick_String(x: String) = ScSome(StringfromFunction0CanvasClick_String(x))
 }
 //</editor-fold>
 
--- macrojs/src/main/scala/com/simplesys/macros/PropsToDictionary.scala
+++ macrojs/src/main/scala/com/simplesys/macros/PropsToDictionary.scala
@@ -83,7 +83,7 @@ object PropsToDictionary extends Logging {
                                                     getTree4DoubleType(typeOf[IntString[_, _]].typeSymbol, q"IntFRomIntString", q"StringFRomIntString") match {
                                                         case None => getTree4DoubleType(typeOf[DoubleAlignment[_, _]].typeSymbol, q"AlignmentfromDoubleAlignment", q"VerticalAlignmentfromDoubleAlignment") match {
                                                             case None => getTree4DoubleType(typeOf[FormItemType_String[_, _]].typeSymbol, q"FormItemTypefromFormItemType_String", q"StringfromFormItemType_String") match {
-                                                                case None => getTree4DoubleType(typeOf[Function1CanvasClick_String[_, _]].typeSymbol, q"Function1CanvasClickfromFunction1CanvasClick_String", q"StringfromFunction1CanvasClick_String")
+                                                                case None => getTree4DoubleType(typeOf[Function0CanvasClick_String[_, _]].typeSymbol, q"Function0CanvasClickfromFunction0CanvasClick_String", q"StringfromFunction0CanvasClick_String")
                                                                 case some => some
                                                             }
                                                             case some => some
@@ -163,7 +163,7 @@ object PropsToDictionary extends Logging {
                     }
                 }"""
         }
-        //logger debug showCode(res.tree)
+        logger debug showCode(res.tree)
         res
     }
 }
