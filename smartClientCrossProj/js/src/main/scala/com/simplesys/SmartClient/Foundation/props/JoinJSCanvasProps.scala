deb6c176aae916a437d9ac7370135cace2212b3e
andrew <uandrew1965@gmail.com>
2016-02-26 16:30:33 +0300

    Доработки

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Foundation/props/JoinJSCanvasProps.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Foundation/props/JoinJSCanvasProps.scala
@@ -1,8 +1,10 @@
 package com.simplesys.SmartClient.Foundation.props
 
-import com.simplesys.option.{ScNone, ScOption}
+import com.simplesys.option.{Function1CanvasClick_String, ScNone, ScOption}
+
+import scala.scalajs.js
 
 class JoinJSCanvasProps extends CanvasProps {
     var suffix: ScOption[String] = ScNone
-    var joinJSFunc: ScOption[String] = ScNone
+    var joinJSFunc: ScOption[Function1CanvasClick_String[js.ThisFunction0[_, _], String]] = ScNone
 }
