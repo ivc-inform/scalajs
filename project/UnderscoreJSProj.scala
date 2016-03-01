263da016e47b3ebe89751f84a54a9a1642451c38
andrew <uandrew1965@gmail.com>
2016-03-01 13:26:18 +0300

    Доработки

---
+++ project/UnderscoreJSProj.scala
@@ -0,0 +1,31 @@
+import com.simplesys.build.{CommonSettings, CommonTypesProj}
+import org.scalajs.sbtplugin.ScalaJSPlugin.AutoImport._
+import sbt.Keys._
+import sbt._
+
+trait UnderscoreJSProj {
+    self: Build with CommonTypesProj =>
+
+    lazy val underscoreJSCrossProj = crossProject.dependsOn(commonTypesCrossProj).
+      settings(
+          name := "underscore-js",
+          version := "1.1-SNAPSHOT",
+
+          libraryDependencies ++= Seq(
+              CommonSettings.cmnDependencies.scalaTest.value
+          ),
+
+          testFrameworks += new TestFramework("utest.runner.Framework"),
+          publishArtifact in(Compile, packageDoc) := false
+      ).
+      jvmSettings().
+      jsSettings(
+          libraryDependencies ++= Seq(
+//              CommonSettings.jsDependencies.scalajsDOM.value,
+//              CommonSettings.jsDependencies.scalajsJQuey.value
+          )
+      ) //.dependsOn().jsConfigure(x => x.dependsOn(macroJS)).jvmConfigure(x => x.dependsOn(macroJVM))
+
+    lazy val underscoreJSCrossJVM = underscoreJSCrossProj.jvm
+    lazy val underscoreJSCrossJS = underscoreJSCrossProj.js
+}
