56c2896ca4a4b00604ef67d7fd5984152d3d6724
andrew <uandrew1965@gmail.com>
2016-03-01 16:07:02 +0300

    Доработки

--- backboneJSCrossProj/js/src/test/scala/backbone/TestBackbone.scala
+++ backboneJSCrossProj/js/src/test/scala/backbone/TestBackbone.scala
@@ -1,5 +1,26 @@
 package backbone
 
-object TestBackbone extends TestSuite{
+import com.simplesys.backbone.Events
+import utest._
+import utest.framework.TestSuite
 
+object TestBackbone extends TestSuite {
+    val tests = TestSuite {
+        'testEvents {
+            val obj = new Events()
+            obj.on("alert", (eventName: String) => scalajs.js.Dynamic.global.alert("Triggered " + eventName))
+
+            obj.trigger("alert", "an event")
+
+            val onChange = () => scalajs.js.Dynamic.global.alert("whatever")
+            val context: scalajs.js.Any = null
+
+            obj.off("change", onChange)
+            obj.off("change")
+            obj.off(null, onChange)
+            obj.off(null, null, context)
+            obj.off()
+        }
+    }
 }
+
