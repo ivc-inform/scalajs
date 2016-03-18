85d64d6b2d9dfdce67db171ae4b36f92cbc0a246
andrew <uandrew1965@gmail.com>
2016-03-18 20:43:11 +0300

    Доработки

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Grids/props/treeGrid/TreeNodeProps.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Grids/props/treeGrid/TreeNodeProps.scala
@@ -15,7 +15,7 @@ class TreeNodeProps extends AbstractClassProps {
     var enabled: ScOption[Boolean] = ScNone
     var icon: ScOption[SCImgURL] = ScNone
     var id: ScOption[String] = ScNone
-    var isFolder: ScOption[Option[Boolean]] = ScNone
+    var isFolder: ScOption[Boolean] = ScNone
     var name: ScOption[String] = ScNone
     var parentId: ScOption[String] = ScNone
     var showDropIcon: ScOption[Boolean] = ScNone
