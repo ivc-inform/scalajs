100d58f52c0aaa04af0a42e65be8b4385eead004
andrew <uandrew1965@gmail.com>
2016-03-18 18:22:30 +0300

    Доработки

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Layout/TabSetSS.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Layout/TabSetSS.scala
@@ -5,9 +5,12 @@ import com.simplesys.System.JSArray
 import com.simplesys.System.Types.void
 
 import scala.scalajs.js
+import scala.scalajs.js.annotation.JSName
 
 @js.native
 trait TabSetSS extends TabSet {
+    @JSName("tabsReordered")
+    var tabsReorderedSS: js.ThisFunction0[TabSetSS, _]
     def filter4Visiblity(tabs: JSArray[Tab]): JSArray[Tab]
     def addTabs(tabs: JSArray[Tab], position: Int = js.native): void
     def addTab(tab: Tab, position: Int = js.native): void
