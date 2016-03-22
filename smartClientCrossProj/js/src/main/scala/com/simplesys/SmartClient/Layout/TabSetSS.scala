e5580fd4f9dfdefe8a03c3ae4880ed72c9b5d225
andrew <uandrew1965@gmail.com>
2016-03-22 15:45:39 +0300

    Доработки

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Layout/TabSetSS.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Layout/TabSetSS.scala
@@ -2,10 +2,11 @@ package com.simplesys.SmartClient.Layout
 
 import com.simplesys.SmartClient.Layout.tabSet.Tab
 import com.simplesys.System.JSArray
-import com.simplesys.System.Types.void
+import com.simplesys.System.Types.{ID, void}
 
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSName
+import scala.scalajs.js.|
 
 @js.native
 trait TabSetSS extends TabSet {
@@ -14,7 +15,8 @@ trait TabSetSS extends TabSet {
     def filter4Visiblity(tabs: JSArray[Tab]): JSArray[Tab]
     def addTabs(tabs: JSArray[Tab], position: Int = js.native): void
     def addTab(tab: Tab, position: Int = js.native): void
-    def removeTab(tab: Tab, position: Int = js.native): void
+    def removeTab(tab: Tab | Int | ID, position: Int = js.native): void
+    def removeAllTabs(): void
     def getLength(): Int
     def isEmpty(): Boolean
     def isNotEmpty(): Boolean
