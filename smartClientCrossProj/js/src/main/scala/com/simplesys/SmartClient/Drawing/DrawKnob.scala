f7e7c646bddb6010908040ff5391254599f31b44
andrew <uandrew1965@gmail.com>
2016-03-23 17:01:59 +0300

    Доработки

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Drawing/DrawKnob.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Drawing/DrawKnob.scala
@@ -1,10 +1,18 @@
 package com.simplesys.SmartClient.Drawing
 
-import com.simplesys.SmartClient.Foundation.{AbstractCanvasCompanion, Canvas}
+import com.simplesys.SmartClient.Foundation.Canvas
+import com.simplesys.System.Types.{AutoChild, void}
 
 import scala.scalajs.js
 
 @js.native
 trait DrawKnob extends Canvas {
+    val drawPane: DrawPane
+    val knobShape: DrawItem with AutoChild
+    val knobShapeDefaults: DrawItem
+    def setCenterPoint(x: Int, y: Int, viewboxCoords: Boolean = js.native): void
+    var updatePoints: js.Function5[Int, Int, Int, Int, String, _]
+    val x: Int
+    val y: Int
 }
 
