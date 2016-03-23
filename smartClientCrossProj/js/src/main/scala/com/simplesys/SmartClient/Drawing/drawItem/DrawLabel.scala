cf57a9482c2da461cf20f29b7cb0c99dc504ddc4
andrew <uandrew1965@gmail.com>
2016-03-23 15:53:46 +0300

    Доработки

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Drawing/drawItem/DrawLabel.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Drawing/drawItem/DrawLabel.scala
@@ -1,15 +1,31 @@
 package com.simplesys.SmartClient.Drawing.drawItem
 
 import com.simplesys.SmartClient.Drawing.{AbstractDrawItemCompanion, DrawItem}
+import com.simplesys.System.Types.{CSSColor, void}
 
 import scala.scalajs.js
 
 @js.native
 trait DrawLabel extends DrawItem {
+    val alignment: String
+    var contents: String
+    val fontFamily: String
+    var fontSize:Int
+    val fontStyle: String
+    val fontWeight: String
+    val left: Int
+    def setContents (contents: String): void
+    def setFontSize (size: Int): void
+    val top: Int
 }
 
 @js.native
 abstract trait AbstractDrawLabelCompanion extends AbstractDrawItemCompanion {
+    var defaultCursiveFont: String = js.native
+    var defaultFantasyFont: String = js.native
+    var defaultMonospaceFont: String = js.native
+    var defaultSansSerifFont: String = js.native
+    var defaultSerifFont: String = js.native
 }
 
 @js.native
