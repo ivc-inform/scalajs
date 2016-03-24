57db5cf10ec53ed1726b960be333cd0898097883
andrew <uandrew1965@gmail.com>
2016-03-24 18:30:40 +0300

    Доработки

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/System/SCApply.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/System/SCApply.scala
@@ -1,7 +1,7 @@
 package com.simplesys.SmartClient.System
 
 import com.simplesys.System.{JSDictionaryAny, JSObject}
-import com.simplesys.json.Json._
+import com.simplesys.json.JSON._
 import com.simplesys.macros.PropsToDictionary
 import com.simplesys.props.AbstractClassProps
 
