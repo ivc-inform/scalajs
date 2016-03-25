407ab1781af1e1a4327ba5402b0c767bd6f94c8a
andrew <uandrew1965@gmail.com>
2016-03-25 11:42:22 +0300

    Доработки

--- commonTypesCrossProj/js/src/main/scala/com/simplesys/System/package.scala
+++ commonTypesCrossProj/js/src/main/scala/com/simplesys/System/package.scala
@@ -8,7 +8,9 @@ package object System {
     type JSDynamic = js.Dynamic
     type JSObject = js.Object
     type JSArray[T] = js.Array[T]
+    type JSUndefined[T] = js.UndefOr[T]
     type JSDictionary[T] = js.Dictionary[T]
     type JSArrayAny = JSArray[JSAny]
     type JSDictionaryAny = JSDictionary[JSAny]
+    val jSUndefined = js.undefined
 }
