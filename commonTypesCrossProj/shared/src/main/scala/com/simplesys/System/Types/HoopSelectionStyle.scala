4d456ec25cb863bb060949b31a6ae8fa24936a3c
andrew <uandrew1965@gmail.com>
2016-04-02 17:23:25 +0300

    .

---
+++ commonTypesCrossProj/shared/src/main/scala/com/simplesys/System/Types/HoopSelectionStyle.scala
@@ -0,0 +1,8 @@
+package com.simplesys.System.Types
+
+import language.implicitConversions
+
+object HoopSelectionStyle extends Enumeration {
+    type HoopSelectionStyle = Value
+    val encloses, intersects = Value
+}
