f4aa05780051283d64db7e3165c2ca24f727c866
andrew <uandrew1965@gmail.com>
2016-04-25 19:23:54 +0300

    .

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Drawing/Point.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Drawing/Point.scala
@@ -1,10 +1,8 @@
 package com.simplesys.SmartClient.Drawing
 
-import com.simplesys.SmartClient.System.Point
-
 import scala.scalajs.js
 
 object Point {
-    def apply(x: Int, y: Int): Point = js.Array(x, y)
+    def apply(x: Int, y: Int) = js.Array(x, y)
 }
 
