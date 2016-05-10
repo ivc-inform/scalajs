319b7f03b2d469283193db2aa8f458ed68563072
andrew <uandrew1965@gmail.com>
2016-05-10 16:16:39 +0300

    .

---
+++ commonTypesCrossProj/shared/src/main/scala/com/simplesys/System/Types/ContentsSSType.scala
@@ -0,0 +1,8 @@
+package com.simplesys.System.Types
+
+import language.implicitConversions
+
+object ContentsSSType extends Enumeration {
+    type ContentsSSType = Value
+    val `any`, orthogonal = Value
+}
