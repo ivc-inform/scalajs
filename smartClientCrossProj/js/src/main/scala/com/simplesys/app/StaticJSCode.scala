eedddecebd2a76d5978bc85d2d157c389f4310fd
andrew <uandrew1965@gmail.com>
2016-06-03 13:45:13 +0300

    .

---
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/app/StaticJSCode.scala
@@ -0,0 +1,5 @@
+package com.simplesys.js.com.simplesys.app
+
+abstract class StaticJSCode {
+  def createJS(): Unit
+}
---
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/app/simpleSyS.scala
@@ -0,0 +1,10 @@
+package com.simplesys.js.com.simplesys.app
+
+import com.simplesys.System.JSObject
+
+import scala.scalajs.js
+
+@js.native
+object simpleSyS extends JSObject{
+    def guid(): String = js.native
+}
