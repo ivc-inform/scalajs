be79e48d58ff2ca523be3583f7f62ecea68e2b7e
andrew <uandrew1965@gmail.com>
2016-06-25 18:09:09 +0300

    .

---
+++ commonTypesCrossProj/js/src/main/scala/com/simplesys/System/NameStrong.scala
@@ -0,0 +1,9 @@
+package com.simplesys.System
+
+import scala.scalajs.js.annotation.ScalaJSDefined
+
+@ScalaJSDefined
+trait NameStrong extends JSObject {
+    val name: String
+}
+
--- smartClientCrossProj/js/src/main/scala/com/simplesys/app/StaticJSCode.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/app/StaticJSCode.scala
@@ -1,13 +1,5 @@
 package com.simplesys.js.com.simplesys.app
 
-import com.simplesys.System.NameStrong
-
-import scala.scalajs.js.annotation.ScalaJSDefined
-
-class eakd_abonents_id_NameStrong extends NameStrong {
-    val name = "id"
-}
-
 abstract class StaticJSCode {
     def createJS(): Unit
 }
