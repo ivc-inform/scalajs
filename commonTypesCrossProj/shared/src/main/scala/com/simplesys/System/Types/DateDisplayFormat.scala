70c09fff2582eca664fd7016ecf148888ccb6a7d
andrew <uandrew1965@gmail.com>
2016-07-01 14:44:40 +0300

    Merge branch '1.1' of http://toucan.simplesys.lan/scm-manager/git/experimental/scalajs into 1.1
    # Conflicts:
    #	smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Layout/RibbonGroupSS.scala
    #	smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Layout/props/RibbonGroupSSProps.scala

--- commonTypesCrossProj/shared/src/main/scala/com/simplesys/System/Types/DateDisplayFormat.scala
+++ commonTypesCrossProj/shared/src/main/scala/com/simplesys/System/Types/DateDisplayFormat.scala
@@ -2,5 +2,5 @@ package com.simplesys.System.Types
 
 object DateDisplayFormat extends Enumeration {
     type DateDisplayFormat = Value
-    val toLocaleString, toUSShortDate, toUSShortDatetime, toEuropeanShortDate, toEuropeanShortDatetime, toJapanShortDate, toJapanShortDatetime, toSerializeableDate, toDateStamp = Value
+    val toLocaleString, toUSShortDate, toUSShortDatetime, toEuropeanShortDate, toEuropeanShortDatetime, toEuropeanDatetime, toJapanShortDate, toJapanShortDatetime, toSerializeableDate, toDateStamp = Value
 }
