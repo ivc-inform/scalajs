912d7dcc3f06935350a6eca6ae26dc06f73b0acd
andrew <uandrew1965@gmail.com>
2016-09-14 19:11:47 +0300

    .

--- smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Drawing/DrawItemDefaults.scala
+++ smartClientCrossProj/js/src/main/scala/com/simplesys/SmartClient/Drawing/DrawItemDefaults.scala
@@ -138,32 +138,3 @@ trait DrawPaneDefaults extends DrawItemDefaults {
     var gradients: IscArray[SimpleGradient]
 }
 
-@ScalaJSDefined
-trait PropEditorLiveObject extends JSObject {
-
-    var ID: JSUndefined[String]
-    var serializeID: JSUndefined[String]
-    var canDrag: JSUndefined[Boolean]
-    var cursor: JSUndefined[String]
-    var endArrow: JSUndefined[String]
-    var fillColor: JSUndefined[String]
-    var fillGradient: JSUndefined[JSObject]
-    var fillOpacity: JSUndefined[Double]
-    val lineCap: JSUndefined[String]
-    val lineColor: JSUndefined[String]
-    val lineOpacity: JSUndefined[Double]
-    val linePattern: JSUndefined[String]
-    val lineWidth: JSUndefined[Int]
-    val shadow: JSUndefined[JSObject]
-    val criteria: JSUndefined[JSObject]
-    val startArrow: JSUndefined[String]
-    val titleRotationMode: JSUndefined[String]
-    val title: JSUndefined[String]
-    val rounding: JSUndefined[Double]
-    val keepInParentRect: JSUndefined[Boolean]
-    val multiline: JSUndefined[Boolean]
-    val listRefs: JSUndefined[JSObject]
-    val titleLabelProperties: JSUndefined[DrawLabel]
-    val messageSMS: JSUndefined[String]
-    val timerProps: JSUndefined[JSObject]
-}
