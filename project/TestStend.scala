6f0cd6356f817988b59d4c8c7829561d30f5aae5
andrew <uandrew1965@gmail.com>
2016-11-09 16:56:24 +0300

    .

--- project/TestStend.scala
+++ project/TestStend.scala
@@ -16,7 +16,7 @@ trait TestStend {
 
 
     lazy val testStend = crossProject.dependsOn(smartClientCrossProj).enablePlugins(
-        ScalaJSPlugin, JettyPlugin, MergeWebappPlugin
+        ScalaJSPlugin, JettyPlugin
     ).
       settings(
           name := "test-stend",
@@ -29,17 +29,7 @@ trait TestStend {
           publishArtifact in(Compile, packageDoc) := false,
           containerPort := 8084,
           containerArgs := Seq("--path", "/test-stend"),
-          containerLibs in Jetty := Seq(("org.eclipse.jetty" % "jetty-runner" % versions.jettyVersion).intransitive()),
-
-          //merger
-          mergeMapping in MergeWebappConfig := Seq(
-              ("com.simplesys", "smartclient-js") -> Seq(
-                  Seq("isomorphic") -> Some(Seq("webapp", "isomorphic"))
-              )
-          ),
-          currentProjectGenerationDirPath in MergeWebappConfig := (sourceDirectory in Compile).value / "webapp" / "javascript" / "generated" / "generatedComponents",
-          currentProjectDevelopedDirPath in MergeWebappConfig := (sourceDirectory in Compile).value / "webapp" / "javascript" / "developed",
-          currentProjectCoffeeDevelopedDirPath in MergeWebappConfig := (sourceDirectory in Compile).value / "webapp" / "coffeescript" / "developed"
+          containerLibs in Jetty := Seq(("org.eclipse.jetty" % "jetty-runner" % versions.jettyVersion).intransitive())
       ).
       jvmSettings(
           libraryDependencies ++= {
@@ -51,8 +41,19 @@ trait TestStend {
           crossTarget in fastOptJS := (sourceDirectory in Compile).value / "webapp" / "javascript" / "generated" / "generatedComponents",
           crossTarget in fullOptJS := (sourceDirectory in Compile).value / "webapp" / "javascript" / "generated" / "generatedComponents",
           crossTarget in packageJSDependencies := (sourceDirectory in Compile).value / "webapp" / "javascript" / "generated" / "generatedComponents",
+
+          //merger
+          mergeMapping in MergeWebappConfig := Seq(
+              ("com.simplesys", "smartclient-js") -> Seq(
+                  Seq("isomorphic") -> Some(Seq("webapp", "isomorphic"))
+              )
+          ),
+          currentProjectGenerationDirPath in MergeWebappConfig := (sourceDirectory in Compile).value / "webapp" / "javascript" / "generated" / "generatedComponents",
+          currentProjectDevelopedDirPath in MergeWebappConfig := (sourceDirectory in Compile).value / "webapp" / "javascript" / "developed",
+          currentProjectCoffeeDevelopedDirPath in MergeWebappConfig := (sourceDirectory in Compile).value / "webapp" / "coffeescript" / "developed",
+
           libraryDependencies ++= Seq()
-      ).dependsOn(smartClientCrossProj).jsConfigure(x => x.dependsOn(smartClientJS)).jvmConfigure(x => x.dependsOn(smartClientJVM))
+      ).dependsOn(smartClientCrossProj).jsConfigure(x => x.dependsOn(smartClientJS).enablePlugins(MergeWebappPlugin)).jvmConfigure(x => x.dependsOn(smartClientJVM))
 
     // Needed, so sbt finds the projects
     lazy val testStendJVM = testStend.jvm
